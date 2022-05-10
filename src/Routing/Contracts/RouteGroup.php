eb6d1a2558e43928750e5b20eb0e36e2bf5324b8
Jakub Theimer <jakub@dy.st>
2022-05-11 00:09:18 +0200

    add route group contract

---
+++ src/Routing/Contracts/RouteGroup.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Dystcz\GetcandyApi\Routing\Contracts;
+
+interface RouteGroup
+{
+    /**
+     * Register routes.
+     *
+     * @param null|string $prefix
+     * @param array|string $middleware
+     * @return void
+     */
+    public function routes(?string $prefix = null, array|string $middleware = []): void;
+}
