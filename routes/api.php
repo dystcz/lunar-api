0ede316f4804611236993ce976afa2e9cf16cf45
Jakub Theimer <jakub@dy.st>
2022-05-11 00:10:13 +0200

    register product routes

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
+use Dystcz\GetcandyApi\Routing\RouteGroups\ProductsRouteGroup;
+use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => Config::get('getcandy-api.route_prefix')], function () {
-    Route::group([
-        'prefix' => 'products',
-    ], __DIR__.'/includes/products.php');
+Route::group([
+    'prefix' => Config::get('getcandy-api.route_prefix'),
+    'middleware' => Config::get('getcandy-api.route_middleware'),
+], function (Router $router) {
+    (new ProductsRouteGroup)->routes();
 });
