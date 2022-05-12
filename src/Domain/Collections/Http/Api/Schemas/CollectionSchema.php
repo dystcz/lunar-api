669c83ee333f91581d1ad03fb58158ffce756baf
Jakub Theimer <jakub@dy.st>
2022-05-12 02:38:20 +0200

    add colleciton schema

---
+++ src/Domain/Collections/Http/Api/Schemas/CollectionSchema.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Dystcz\GetcandyApi\Domain\Collections\Http\Api\Schemas;
+
+use GoldSpecDigital\ObjectOrientedOAS\Contracts\SchemaContract;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\AllOf;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\AnyOf;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Not;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\OneOf;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Schema;
+use Vyuldashev\LaravelOpenApi\Contracts\Reusable;
+use Vyuldashev\LaravelOpenApi\Factories\SchemaFactory;
+
+class CollectionSchema extends SchemaFactory implements Reusable
+{
+    /**
+     * @return AllOf|OneOf|AnyOf|Not|Schema
+     */
+    public function build(): SchemaContract
+    {
+        return Schema::object('Collection')
+            ->properties(
+                Schema::string('foo')->default(null),
+                Schema::integer('bar')->default(0),
+            );
+    }
+}
