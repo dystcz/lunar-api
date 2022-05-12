50e01758c1ea2860b19baf9c8e97774a1bbce29d
Jakub Theimer <jakub@dy.st>
2022-05-12 02:39:30 +0200

    add show collection response

---
+++ src/Domain/Collections/Http/Api/Responses/ShowCollectionResponse.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Dystcz\GetcandyApi\Domain\Collections\Http\Api\Responses;
+
+use Dystcz\GetcandyApi\Domain\Collections\Http\Api\Schemas\CollectionSchema;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\MediaType;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Response;
+use Vyuldashev\LaravelOpenApi\Factories\ResponseFactory;
+
+class ShowCollectionResponse extends ResponseFactory
+{
+    public function build(): Response
+    {
+        return Response::ok()->description('Successful response')->content(
+            MediaType::json()->schema(CollectionSchema::ref())
+        );
+    }
+}
