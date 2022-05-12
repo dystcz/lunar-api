e00ba871071bdedb6a1a6e9d4cfaf7a2159b5b22
Jakub Theimer <jakub@dy.st>
2022-05-12 02:39:09 +0200

    add collection resource

---
+++ src/Domain/Collections/Http/Resources/CollectionResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Dystcz\GetcandyApi\Domain\Collections\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CollectionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
