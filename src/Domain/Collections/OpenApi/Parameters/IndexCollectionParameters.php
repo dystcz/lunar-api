ab5c7121f8fba2be961e2eb0dfbaa29abe39622b
lorinczdev <lorinczdev@users.noreply.github.com>
2022-11-18 12:50:47 +0000

    Fix styling

--- src/Domain/Collections/OpenApi/Parameters/IndexCollectionParameters.php
+++ src/Domain/Collections/OpenApi/Parameters/IndexCollectionParameters.php
@@ -4,7 +4,6 @@ namespace Dystcz\LunarApi\Domain\Collections\OpenApi\Parameters;
 
 use Dystcz\LunarApi\Domain\JsonApi\Builders\CollectionJsonApiBuilder;
 use GoldSpecDigital\ObjectOrientedOAS\Objects\Parameter;
-use GoldSpecDigital\ObjectOrientedOAS\Objects\Schema;
 use Vyuldashev\LaravelOpenApi\Factories\ParametersFactory;
 
 class IndexCollectionParameters extends ParametersFactory
