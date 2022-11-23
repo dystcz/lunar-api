c5225301aee3c9326930db3df32c9058bdc06626
Jakub Theimer <jakub@dy.st>
2022-11-23 19:00:47 +0100

    update media resource

--- src/Domain/Media/Http/Resources/MediaResource.php
+++ src/Domain/Media/Http/Resources/MediaResource.php
@@ -4,13 +4,24 @@ namespace Dystcz\LunarApi\Domain\Media\Http\Resources;
 
 use Dystcz\LunarApi\Domain\JsonApi\Http\Resources\JsonApiResource;
 use Illuminate\Http\Request;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class MediaResource extends JsonApiResource
 {
     protected function toAttributes(Request $request): array
     {
+        /** @var Media $model */
+        $model = $this->resource;
+
         return [
-            'url' => $this->getFullUrl(),
+            'id' => $model->id,
+            'path' => "{$model->id}/{$model->file_name}",
+            'url' => $model->getFullUrl(),
+            'name' => $model->file_name,
+            'type' => $model->mime_type,
+            'size' => $model->size,
+            'collection_name' => $model->collection_name,
+            'order' => $model->order_column,
         ];
     }
 }
