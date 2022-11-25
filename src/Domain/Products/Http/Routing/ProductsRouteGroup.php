ca893a862a42192f581128fa0a9e3d1e1627ec57
Jakub Theimer <jakub@dy.st>
2022-11-25 17:18:43 +0100

    use json api controller

--- src/Domain/Products/Http/Routing/ProductsRouteGroup.php
+++ src/Domain/Products/Http/Routing/ProductsRouteGroup.php
@@ -6,6 +6,8 @@ use Dystcz\LunarApi\Domain\Products\Http\Controllers\ProductsController;
 use Dystcz\LunarApi\Routing\Contracts\RouteGroup as RouteGroupContract;
 use Dystcz\LunarApi\Routing\RouteGroup;
 use Illuminate\Support\Facades\Route;
+use LaravelJsonApi\Laravel\Facades\JsonApiRoute;
+use LaravelJsonApi\Laravel\Http\Controllers\JsonApiController;
 
 class ProductsRouteGroup extends RouteGroup implements RouteGroupContract
 {
@@ -25,11 +27,15 @@ class ProductsRouteGroup extends RouteGroup implements RouteGroupContract
     public function routes(?string $prefix = null, array|string $middleware = []): void
     {
         $this->router->group([
-            'prefix' => $this->getPrefix($prefix),
+            // 'prefix' => $this->getPrefix($prefix),
             'middleware' => $this->getMiddleware($middleware),
         ], function () {
-            Route::get('/', [ProductsController::class, 'index'])->name("{$this->prefix}.index");
-            Route::get('/{slug}', [ProductsController::class, 'show'])->name("{$this->prefix}.show");
+            // Route::get('/', [ProductsController::class, 'index'])->name("{$this->prefix}.index");
+            // Route::get('/{slug}', [ProductsController::class, 'show'])->name("{$this->prefix}.show");
+
+            JsonApiRoute::server('v1')->prefix('v1')->resources(function ($server) {
+                $server->resource('products', JsonApiController::class)->readOnly();
+            });
         });
     }
 }
