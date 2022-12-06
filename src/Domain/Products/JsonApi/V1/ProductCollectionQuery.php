298cf9aaab612bd052a7bf29937ecad7995e5b36
Jakub Theimer <jakub@dy.st>
2022-12-06 16:13:40 +0700

    add products v1 classes

---
+++ src/Domain/Products/JsonApi/V1/ProductCollectionQuery.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Dystcz\LunarApi\Domain\Products\JsonApi\V1;
+
+use LaravelJsonApi\Laravel\Http\Requests\ResourceQuery;
+use LaravelJsonApi\Validation\Rule as JsonApiRule;
+
+class ProductCollectionQuery extends ResourceQuery
+{
+    /**
+     * Get the validation rules that apply to the request query parameters.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'fields' => [
+                'nullable',
+                'array',
+                JsonApiRule::fieldSets(),
+            ],
+            'filter' => [
+                'nullable',
+                'array',
+                JsonApiRule::filter(),
+            ],
+            'include' => [
+                'nullable',
+                'string',
+                JsonApiRule::includePaths(),
+            ],
+            'page' => [
+                'nullable',
+                'array',
+                JsonApiRule::page(),
+            ],
+            'sort' => [
+                'nullable',
+                'string',
+                JsonApiRule::sort(),
+            ],
+            '{{ withCount }}' => [
+                'nullable',
+                'string',
+                JsonApiRule::countable(),
+            ],
+        ];
+    }
+}
---
+++ src/Domain/Products/JsonApi/V1/ProductQuery.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Dystcz\LunarApi\Domain\Products\JsonApi\V1;
+
+use LaravelJsonApi\Laravel\Http\Requests\ResourceQuery;
+use LaravelJsonApi\Validation\Rule as JsonApiRule;
+
+class ProductQuery extends ResourceQuery
+{
+    /**
+     * Get the validation rules that apply to the request query parameters.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'fields' => [
+                'nullable',
+                'array',
+                JsonApiRule::fieldSets(),
+            ],
+            'filter' => [
+                'nullable',
+                'array',
+                JsonApiRule::filter()->forget('id'),
+            ],
+            'include' => [
+                'nullable',
+                'string',
+                JsonApiRule::includePaths(),
+            ],
+            'page' => JsonApiRule::notSupported(),
+            'sort' => JsonApiRule::notSupported(),
+            '{{ withCount }}' => [
+                'nullable',
+                'string',
+                JsonApiRule::countable(),
+            ],
+        ];
+    }
+}
