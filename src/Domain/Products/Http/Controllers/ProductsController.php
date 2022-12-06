da51247953136bb142af6e66b144ea451831a053
Jakub Theimer <jakub@dy.st>
2022-12-06 17:16:27 +0700

    remove commented out code

--- src/Domain/Products/Http/Controllers/ProductsController.php
+++ src/Domain/Products/Http/Controllers/ProductsController.php
@@ -2,7 +2,6 @@
 
 namespace Dystcz\LunarApi\Domain\Products\Http\Controllers;
 
-use Dystcz\LunarApi\Domain\JsonApi\Builders\ProductJsonApiBuilder;
 use Dystcz\LunarApi\Domain\OpenApi\Responses\ErrorNotFoundResponse;
 use Dystcz\LunarApi\Domain\Products\JsonApi\V1\ProductCollectionQuery;
 use Dystcz\LunarApi\Domain\Products\JsonApi\V1\ProductQuery;
@@ -14,14 +13,9 @@ use Dystcz\LunarApi\Domain\Products\OpenApi\Responses\ShowProductResponse;
 use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Config;
 use LaravelJsonApi\Core\Responses\DataResponse;
 use LaravelJsonApi\Laravel\Http\Controllers\Actions;
 use Lunar\Models\Product;
-use Spatie\QueryBuilder\QueryBuilder;
-use TiMacDonald\JsonApi\JsonApiResource;
-use TiMacDonald\JsonApi\JsonApiResourceCollection;
 use Vyuldashev\LaravelOpenApi\Attributes as OpenApi;
 
 #[OpenApi\PathItem]
@@ -37,13 +31,6 @@ class ProductsController extends Controller
     // use Actions\UpdateRelationship;
     // use Actions\AttachRelationship;
     // use Actions\DetachRelationship;
-
-    protected QueryBuilder $query;
-
-    public function __construct(ProductJsonApiBuilder $builder)
-    {
-        $this->query = $builder->query();
-    }
 
     /**
      * Fetch zero to many JSON API resources.
@@ -65,31 +52,6 @@ class ProductsController extends Controller
 
         return new DataResponse($models);
     }
-
-    // /**
-    //  * List products.
-    //  *
-    //  * Lists all products based on provided filters.
-    //  *
-    //  * @return JsonApiResourceCollection
-    //  */
-    // #[OpenApi\Operation(tags: ['products'])]
-    // #[OpenApi\Parameters(factory: IndexProductParameters::class)]
-    // #[OpenApi\Response(factory: IndexProductResponse::class, statusCode: 200)]
-    // public function index(): JsonApiResourceCollection
-    // {
-    //     $products = $this
-    //         ->query
-    //         ->with([
-    //             'defaultUrl',
-    //             'productType',
-    //             'productType.mappedAttributes',
-    //             'productType.mappedAttributes.attributeGroup',
-    //         ])
-    //         ->paginate(Config::get('lunar-api.domains.products.pagination', 12));
-    //
-    //     return ProductResource::collection($products);
-    // }
 
     /**
      * Fetch zero to one JSON API resource by id.
@@ -115,33 +77,4 @@ class ProductsController extends Controller
 
         return new DataResponse($model);
     }
-
-    // /**
-    //  * Show product.
-    //  *
-    //  * Show product by slug.
-    //  *
-    //  * @param  string  $slug
-    //  * @return JsonApiResource
-    //  */
-    // #[OpenApi\Operation(tags: ['products'])]
-    // #[OpenApi\Parameters(factory: ShowProductParameters::class)]
-    // #[OpenApi\Response(factory: ShowProductResponse::class, statusCode: 200)]
-    // #[OpenApi\Response(factory: ErrorNotFoundResponse::class, statusCode: 404)]
-    // public function show(string $slug): JsonApiResource
-    // {
-    //     $product = $this->query
-    //         ->whereHas(
-    //             'defaultUrl',
-    //             fn ($query) => $query
-    //                 ->where('slug', $slug)
-    //                 ->whereHas(
-    //                     'language',
-    //                     fn ($query) => $query->where('code', App::getLocale())
-    //                 )
-    //         )
-    //         ->firstOrFail();
-    //
-    //     return new ProductResource($product);
-    // }
 }
