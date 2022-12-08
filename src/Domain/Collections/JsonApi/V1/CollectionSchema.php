caaf476e94f3b11a17aa45f28fd8936b3cb1ad77
Jakub Theimer <jakub@dy.st>
2022-12-09 00:57:02 +0700

    add collection schema

---
+++ src/Domain/Collections/JsonApi/V1/CollectionSchema.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Dystcz\LunarApi\Domain\Collections\JsonApi\V1;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+use LaravelJsonApi\Eloquent\Contracts\Paginator;
+use LaravelJsonApi\Eloquent\Fields\ID;
+use LaravelJsonApi\Eloquent\Fields\Relations\HasMany;
+use LaravelJsonApi\Eloquent\Fields\Relations\HasOne;
+use LaravelJsonApi\Eloquent\Filters\WhereHas;
+use LaravelJsonApi\Eloquent\Filters\WhereIdIn;
+use LaravelJsonApi\Eloquent\Pagination\PagePagination;
+use LaravelJsonApi\Eloquent\Schema;
+use Lunar\Models\Collection;
+
+class CollectionSchema extends Schema
+{
+    /**
+     * The default paging parameters to use if the client supplies none.
+     *
+     * @var array|null
+     */
+    protected ?array $defaultPagination = ['number' => 1];
+
+    /**
+     * The model the schema corresponds to.
+     *
+     * @var string
+     */
+    public static string $model = Collection::class;
+
+    /**
+     * Build an index query for this resource.
+     *
+     * @param Request|null $request
+     * @param Builder $query
+     * @return Builder
+     */
+    public function indexQuery(?Request $request, Builder $query): Builder
+    {
+        return $query;
+    }
+
+    /**
+     * Build a "relatable" query for this resource.
+     *
+     * @param Request|null $request
+     * @param Relation $query
+     * @return Relation
+     */
+    public function relatableQuery(?Request $request, Relation $query): Relation
+    {
+        return $query;
+    }
+
+    /**
+     * The relationships that should always be eager loaded.
+     *
+     * @return array
+     */
+    public function with(): array
+    {
+        return [
+            'defaultUrl',
+        ];
+    }
+
+    /**
+     * Get the include paths supported by this resource.
+     *
+     * @return string[]|iterable
+     */
+    public function includePaths(): iterable
+    {
+        return [
+            'products',
+            'products.urls',
+            'products.default-urls',
+            'products.images',
+            'urls',
+            'default-urls',
+        ];
+    }
+
+    /**
+     * Get the resource fields.
+     *
+     * @return array
+     */
+    public function fields(): array
+    {
+        return [
+            ID::make(),
+
+            HasMany::make('products'),
+            HasOne::make('default-urls', 'defaultUrl'),
+            HasMany::make('urls'),
+        ];
+    }
+
+    /**
+     * Get the resource filters.
+     *
+     * @return array
+     */
+    public function filters(): array
+    {
+        return [
+            WhereIdIn::make($this),
+
+            WhereHas::make($this, 'default-urls', 'url')->singular(),
+        ];
+    }
+
+    /**
+     * Will the set of filters result in zero-to-one resource?
+     *
+     * While individual filters can be marked as singular, there may be instances
+     * where the combination of filters should result in a singular response
+     * (zero-to-one resource instead of zero-to-many). Developers can use this
+     * hook to add complex logic for working out if a set of filters should
+     * return a singular resource.
+     *
+     * @param array $filters
+     * @return bool
+     */
+    public function isSingular(array $filters): bool
+    {
+        // return isset($filters['userId'], $filters['clientId']);
+
+        return false;
+    }
+
+    /**
+     * Get the resource paginator.
+     *
+     * @return Paginator|null
+     */
+    public function pagination(): ?Paginator
+    {
+        return PagePagination::make()
+            ->withDefaultPerPage(Config::get('lunar-api.domains.collections.pagination', 12));
+    }
+
+    /**
+     * Get the JSON:API resource type.
+     *
+     * @return string
+     */
+    public static function type(): string
+    {
+        return 'collections';
+    }
+}
