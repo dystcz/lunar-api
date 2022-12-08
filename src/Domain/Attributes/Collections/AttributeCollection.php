d77594c67c9501b406c9ca05bb5fb88756757e67
Jakub Theimer <jakub@dy.st>
2022-12-09 01:01:06 +0700

    revert to simple structure

--- src/Domain/Attributes/Collections/AttributeCollection.php
+++ src/Domain/Attributes/Collections/AttributeCollection.php
@@ -22,12 +22,8 @@ class AttributeCollection extends EloquentCollection
                 fn ($attribute) => $attribute->attributeGroup->handle
             )
             ->map(
-                fn ($group) => $group->map(
-                    fn ($attribute) => [
-                        'key' => $attribute->handle,
-                        'name' => $attribute->translate('name'),
-                        'value' => $model->attr($attribute->handle),
-                    ]
+                fn ($group) => $group->mapWithKeys(
+                    fn ($attribute) => [$attribute->translate('name') => $model->attr($attribute->handle)]
                 )
             );
     }
