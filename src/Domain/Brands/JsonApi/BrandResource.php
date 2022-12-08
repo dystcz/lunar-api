bcebecfff20996fd8a0c169ef08a7c3c7f1dcbfc
Jakub Theimer <jakub@dy.st>
2022-12-08 15:30:31 +0700

    add brand resource

---
+++ src/Domain/Brands/JsonApi/BrandResource.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Dystcz\LunarApi\Domain\Brands\JsonApi;
+
+use LaravelJsonApi\Core\Resources\JsonApiResource;
+use Lunar\Models\Brand;
+
+class BrandResource extends JsonApiResource
+{
+    /**
+     * Get the resource's attributes.
+     *
+     * @param \Illuminate\Http\Request|null $request
+     * @return iterable
+     */
+    public function attributes($request): iterable
+    {
+        /** @var Brand */
+        $model = $this->resource;
+
+        return [
+            'name' => $model->name,
+        ];
+    }
+
+    /**
+     * Get the resource's relationships.
+     *
+     * @param \Illuminate\Http\Request|null $request
+     * @return iterable
+     */
+    public function relationships($request): iterable
+    {
+        return [];
+    }
+}
