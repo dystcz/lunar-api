1d642333fcedaaf45310b4a457239cba2ddef061
Jakub Theimer <jakub@dy.st>
2022-12-09 00:56:50 +0700

    add collection collection query

---
+++ src/Domain/Collections/JsonApi/V1/CollectionCollectionQuery.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Dystcz\LunarApi\Domain\Collections\JsonApi\V1;
+
+use LaravelJsonApi\Laravel\Http\Requests\ResourceQuery;
+use LaravelJsonApi\Validation\Rule as JsonApiRule;
+
+class CollectionCollectionQuery extends ResourceQuery
+{
+    /**
+     * The default include paths to use if the client provides none.
+     *
+     * @var array|null
+     */
+    protected ?array $defaultIncludePaths = [];
+
+    /**
+     * Get the validation rules that apply to the request query parameters.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'fields' => [
+                'nullable',
+                'array',
+                JsonApiRule::fieldSets(),
+            ],
+            'filter' => [
+                'nullable',
+                'array',
+                JsonApiRule::filter(),
+            ],
+            'include' => [
+                'nullable',
+                'string',
+                JsonApiRule::includePaths(),
+            ],
+            'page' => [
+                'nullable',
+                'array',
+                JsonApiRule::page(),
+            ],
+            'sort' => [
+                'nullable',
+                'string',
+                JsonApiRule::sort(),
+            ],
+            'withCount' => [
+                'nullable',
+                'string',
+                JsonApiRule::countable(),
+            ],
+        ];
+    }
+}
