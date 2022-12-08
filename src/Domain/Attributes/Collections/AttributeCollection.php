01965f7ca5396bb505e5607a1c976ea8d66718cc
Jakub Theimer <jakub@dy.st>
2022-12-09 01:12:25 +0700

    refactor again, so its consistent

--- src/Domain/Attributes/Collections/AttributeCollection.php
+++ src/Domain/Attributes/Collections/AttributeCollection.php
@@ -22,21 +22,14 @@ class AttributeCollection extends EloquentCollection
                 fn ($attribute) => $attribute->attributeGroup->handle
             )
             ->map(
-                function ($attributes, $group) use ($model) {
-                    if (in_array($group, ['product_properties'])) {
-                        return $attributes->map(
-                            fn ($attribute) => [
-                                'key' => $attribute->handle,
-                                'name' => $attribute->translate('name'),
-                                'value' => $model->attr($attribute->handle),
-                            ]
-                        );
-                    }
-
-                    return $attributes->mapWithKeys(
-                        fn ($attribute) => [$attribute->handle => $model->attr($attribute->handle)]
-                    );
-                }
+                fn ($attributes) => $attributes->mapWithKeys(
+                    fn ($attribute) => [
+                        $attribute->handle => [
+                            'name' => $attribute->translate('name'),
+                            'value' => $model->attr($attribute->handle),
+                        ],
+                    ]
+                )
             );
     }
 }
