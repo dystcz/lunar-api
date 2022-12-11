e6456944c87a8e34b7abe02be46ce29862976822
Jakub Theimer <jakub@dy.st>
2022-12-12 01:33:35 +0700

    add url route group

---
+++ src/Domain/Urls/Http/Routing/UrlRouteGroup.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Dystcz\LunarApi\Domain\Urls\Http\Routing;
+
+use Dystcz\LunarApi\Routing\Contracts\RouteGroup as RouteGroupContract;
+use Dystcz\LunarApi\Routing\RouteGroup;
+use LaravelJsonApi\Laravel\Facades\JsonApiRoute;
+use LaravelJsonApi\Laravel\Http\Controllers\JsonApiController;
+
+class UrlRouteGroup extends RouteGroup implements RouteGroupContract
+{
+    /** @var string */
+    public string $prefix = 'urls';
+
+    /** @var array */
+    public array $middleware = [];
+
+    /**
+     * Register routes.
+     *
+     * @param  null|string  $prefix
+     * @param  array|string  $middleware
+     * @return void
+     */
+    public function routes(?string $prefix = null, array|string $middleware = []): void
+    {
+        JsonApiRoute::server('v1')
+            ->prefix('v1')
+            ->resources(function ($server) {
+                $server->resource($this->getPrefix(), JsonApiController::class)
+                    ->only('index', 'show')
+                    ->readOnly();
+            });
+    }
+}
