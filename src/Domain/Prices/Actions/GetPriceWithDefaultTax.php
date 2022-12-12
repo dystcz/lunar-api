2e13a46521faffe963a6367c6fded448d88f9557
Jakub Theimer <jakub@dy.st>
2022-12-12 16:50:33 +0700

    add get price with default tax action

---
+++ src/Domain/Prices/Actions/GetPriceWithDefaultTax.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Dystcz\LunarApi\Domain\Prices\Actions;
+
+use Lunar\Base\Purchasable;
+use Lunar\DataTypes\Price;
+use Lunar\Facades\Pricing;
+use Lunar\Facades\Taxes;
+
+class GetPriceWithDefaultTax
+{
+    /**
+     * Get Price with default tax.
+     *
+     * @param Purchasable $purchasable
+     * @return Price
+     */
+    public function __invoke(Purchasable $purchasable, Price $price): Price
+    {
+        $price = $price ?? Pricing::for($purchasable)->get()->base->price;
+        $currency = $price->currency;
+        $subTotal = $price->value;
+
+        $taxBreakDown = Taxes::setCurrency($currency)
+            ->setPurchasable($purchasable)
+            ->getBreakdown($subTotal);
+
+        $tax = $taxBreakDown->amounts->sum('price.value');
+
+        $priceWithVat = new Price($subTotal + $tax, $currency);
+
+        return $priceWithVat;
+    }
+}
