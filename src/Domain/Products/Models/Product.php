e2c702aef156d81be4d13f078cb27eecdbea0d78
Jakub Theimer <jakub@dy.st>
2022-12-17 08:05:16 +0700

    style fix

--- src/Domain/Products/Models/Product.php
+++ src/Domain/Products/Models/Product.php
@@ -3,6 +3,8 @@
 namespace Dystcz\LunarApi\Domain\Products\Models;
 
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 use Lunar\Models\Price;
 use Lunar\Models\Product as LunarProduct;
 use Lunar\Models\ProductVariant;
@@ -29,7 +31,7 @@ class Product extends LunarProduct
             );
     }
 
-    public function lowestPrice(): \Illuminate\Database\Eloquent\Relations\HasOneThrough
+    public function lowestPrice(): HasOneThrough
     {
         $pricesTable = $this->prices()->getModel()->getTable();
         $variantsTable = $this->variants()->getModel()->getTable();
@@ -41,37 +43,37 @@ class Product extends LunarProduct
                 'product_id',
                 'priceable_id'
             )
-            ->where($pricesTable.'.id', function ($query) use ($variantsTable, $pricesTable) {
-                $query->select($pricesTable.'.id')
+            ->where($pricesTable . '.id', function ($query) use ($variantsTable, $pricesTable) {
+                $query->select($pricesTable . '.id')
                     ->from($pricesTable)
                     ->where('priceable_type', ProductVariant::class)
                     ->whereIn('priceable_id', function ($query) use ($variantsTable) {
                         $query->select('variants.id')
-                            ->from($variantsTable.' as variants')
-                            ->whereRaw('variants.product_id = '.$variantsTable.'.product_id');
+                            ->from($variantsTable . ' as variants')
+                            ->whereRaw('variants.product_id = ' . $variantsTable . '.product_id');
                     })
-                    ->orderBy($pricesTable.'.price', 'asc')
+                    ->orderBy($pricesTable . '.price', 'asc')
                     ->limit(1);
             });
     }
 
-    public function cheapestVariant(): \Illuminate\Database\Eloquent\Relations\HasOne
+    public function cheapestVariant(): HasOne
     {
         $pricesTable = $this->prices()->getModel()->getTable();
         $variantsTable = $this->variants()->getModel()->getTable();
 
         return $this
             ->hasOne(ProductVariant::class)
-            ->where($variantsTable.'.id', function ($query) use ($variantsTable, $pricesTable) {
+            ->where($variantsTable . '.id', function ($query) use ($variantsTable, $pricesTable) {
                 $query
                     ->select('variants.id')
-                    ->from($variantsTable.' as variants')
+                    ->from($variantsTable . ' as variants')
                     ->join($pricesTable, function ($join) {
                         $join->on('priceable_id', '=', 'variants.id')
                             ->where('priceable_type', ProductVariant::class);
                     })
                     ->whereRaw('variants.product_id = lunar_product_variants.product_id')
-                    ->orderBy($pricesTable.'.price', 'asc')
+                    ->orderBy($pricesTable . '.price', 'asc')
                     ->limit(1);
             });
     }
