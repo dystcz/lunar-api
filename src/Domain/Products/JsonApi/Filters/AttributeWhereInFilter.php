b205a3f7fb87da6e0af62860dce3810a8203c383
Jakub Theimer <jakub@dy.st>
2023-01-11 05:16:43 +0700

    add attribute whereIn filter

---
+++ src/Domain/Products/JsonApi/Filters/AttributeWhereInFilter.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Dystcz\LunarApi\Domain\Products\JsonApi\Filters;
+
+use LaravelJsonApi\Eloquent\Contracts\Filter;
+use LaravelJsonApi\Eloquent\Filters\Concerns\DeserializesValue;
+use LaravelJsonApi\Eloquent\Filters\Concerns\HasColumn;
+use LaravelJsonApi\Eloquent\Filters\Concerns\HasDelimiter;
+
+class AttributeWhereInFilter implements Filter
+{
+    use HasColumn;
+    use HasDelimiter;
+    use DeserializesValue;
+
+    /**
+     * @var string
+     */
+    private string $attribute;
+
+    /**
+     * @var string
+     */
+    private string $name;
+
+    /**
+     * Create a new filter.
+     *
+     * @param string $name
+     * @param string $attribute
+     * @return static
+     */
+    public static function make(string $name, string $attribute): self
+    {
+        return new static($name, $attribute);
+    }
+
+    /**
+     * CustomFilter constructor.
+     *
+     * @param string $name
+     * @param string $attribute
+     */
+    public function __construct(string $name, string $attribute)
+    {
+        $this->name = $name;
+        $this->column = 'attribute_data';
+        $this->attribute = $attribute;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function key(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isSingular(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function apply($query, $value)
+    {
+        $column = $query->getModel()->qualifyColumn($this->column());
+        $values = $this->deserialize($value);
+        // dd($values);
+
+        $query = $query->whereIn(
+            "{$column}->{$this->attribute}->value",
+            $values
+        );
+
+        foreach ($values as $value) {
+            $query->orWhereJsonContains(
+                "{$column}->{$this->attribute}->value",
+                $value
+            );
+        }
+
+        return $query;
+    }
+
+    /**
+     * Deserialize the fitler value.
+     *
+     * @param string|array $value
+     * @return array
+     */
+    protected function deserialize($value): array
+    {
+        if ($this->deserializer) {
+            return ($this->deserializer)($value);
+        }
+
+        return $this->toArray($value);
+    }
+}
