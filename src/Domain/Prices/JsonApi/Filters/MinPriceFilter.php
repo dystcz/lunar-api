8ed7bd027f0feac51dd352b5eac7403dde77c9fa
Jakub Theimer <jakub@dy.st>
2023-01-11 05:15:59 +0700

    add min price filter

---
+++ src/Domain/Prices/JsonApi/Filters/MinPriceFilter.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Dystcz\LunarApi\Domain\Prices\JsonApi\Filters;
+
+use Illuminate\Support\Str;
+use LaravelJsonApi\Eloquent\Contracts\Filter;
+use LaravelJsonApi\Eloquent\Filters\Concerns\DeserializesValue;
+use LaravelJsonApi\Eloquent\Filters\Concerns\HasColumn;
+use LaravelJsonApi\Eloquent\Filters\Concerns\IsSingular;
+
+class MinPriceFilter implements Filter
+{
+    use DeserializesValue;
+    use IsSingular;
+    use HasColumn;
+
+    /**
+     * @var string
+     */
+    private string $name;
+
+    /**
+     * @var string
+     */
+    private string $column;
+
+    /**
+     * Create a new filter.
+     *
+     * @param string $name
+     * @param string|null $column
+     * @return static
+     */
+    public static function make(string $name, string $column = null): self
+    {
+        return new static($name, $column);
+    }
+
+    /**
+     * CustomFilter constructor.
+     *
+     * @param string $name
+     * @param string|null $column
+     */
+    public function __construct(string $name, string $column = null)
+    {
+        $this->name = $name;
+        $this->column = $column ?: Str::snake($name);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function key(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function apply($query, $value)
+    {
+        return $query->where(
+            $query->getModel()->qualifyColumn($this->column()),
+            '>=',
+            $this->deserialize($value)
+        );
+    }
+}
