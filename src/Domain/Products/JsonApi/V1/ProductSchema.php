7436770462d31867db9448c659ae13aea208f469
Jakub Theimer <jakub@dy.st>
2023-01-12 01:33:27 +0700

    register dynamic attribute filters

--- src/Domain/Products/JsonApi/V1/ProductSchema.php
+++ src/Domain/Products/JsonApi/V1/ProductSchema.php
@@ -2,8 +2,8 @@
 
 namespace Dystcz\LunarApi\Domain\Products\JsonApi\V1;
 
+use Dystcz\LunarApi\Domain\JsonApi\Contracts\FilterCollection;
 use Dystcz\LunarApi\Domain\JsonApi\Eloquent\Schema;
-use Dystcz\LunarApi\Domain\Products\JsonApi\Filters\AttributeWhereInFilter;
 use Dystcz\LunarApi\Domain\Products\JsonApi\Sorts\RecentlyViewedSort;
 use Dystcz\LunarApi\Domain\Products\Models\Product;
 use Illuminate\Database\Eloquent\Builder;
@@ -172,6 +172,9 @@ class ProductSchema extends Schema
      */
     public function filters(): array
     {
+        /** @var FilterCollection $filterCollection */
+        $filterCollection = Config::get('lunar-api.domains.products.filters');
+
         return [
             ...parent::filters(),
 
@@ -183,16 +186,7 @@ class ProductSchema extends Schema
 
             WhereHas::make($this, 'default_url', 'url')->singular(),
 
-            // Attribute filters
-            AttributeWhereInFilter::make('keyboard_size', 'keyboard_size')->delimiter(','),
-
-            AttributeWhereInFilter::make('switch_manufacturer', 'switch_manufacturer')->delimiter(','),
-
-            AttributeWhereInFilter::make('keyboard_layout', 'keyboard_layout')->delimiter(','),
-
-            AttributeWhereInFilter::make('colors', 'colors')->delimiter(','),
-
-            // AttributeBoolFilter::make('led', 'led'),
+            ...(new $filterCollection)->toArray(),
         ];
     }
 
