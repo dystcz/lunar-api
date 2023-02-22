cc5dc0b66133630b4e34751498b7d28f741ce0cd
Marek Lörincz <lorinczdev@gmail.com>
2023-02-22 15:40:31 +0100

    Fill in country attributes

--- src/Domain/Countries/JsonApi/V1/CountrySchema.php
+++ src/Domain/Countries/JsonApi/V1/CountrySchema.php
@@ -4,6 +4,7 @@ namespace Dystcz\LunarApi\Domain\Countries\JsonApi\V1;
 
 use Dystcz\LunarApi\Domain\JsonApi\Eloquent\Schema;
 use LaravelJsonApi\Eloquent\Fields\ID;
+use LaravelJsonApi\Eloquent\Fields\Str;
 use Lunar\Models\Country;
 
 class CountrySchema extends Schema
@@ -50,6 +51,15 @@ class CountrySchema extends Schema
             ...parent::fields(),
 
             ID::make(),
+            Str::make('name'),
+            Str::make('iso2'),
+            Str::make('iso3'),
+            Str::make('phonecode'),
+            Str::make('capital'),
+            Str::make('currency'),
+            Str::make('native'),
+            Str::make('emoji'),
+            Str::make('emoji_u'),
         ];
     }
 
