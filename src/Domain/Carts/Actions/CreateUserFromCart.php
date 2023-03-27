e2a33ba2766dac799ecf99872ed14d6adeeda01e
Marek Lörincz <lorinczdev@gmail.com>
2023-03-28 00:18:28 +0200

    Create a new customer for the user when there isn't one

--- src/Domain/Carts/Actions/CreateUserFromCart.php
+++ src/Domain/Carts/Actions/CreateUserFromCart.php
@@ -39,10 +39,12 @@ class CreateUserFromCart implements CreatesUserFromCart
         ]);
 
         /** @var Customer $customer */
-        $customer = $user->customers()->create([
+        $customer = $user->customers()->firstOrNew([], [
             'first_name' => $shippingAddress->first_name,
             'last_name' => $shippingAddress->last_name,
         ]);
+
+        $customer->save();
 
         $customer->addresses()->createMany($this->getAddresses($cart));
 
