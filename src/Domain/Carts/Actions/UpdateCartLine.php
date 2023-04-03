efb80b29efeb89d00ec6d71fe24414a3ad7f32f1
Jakub Theimer <jakub@dy.st>
2023-04-04 00:03:25 +0200

    add update cart line action

---
+++ src/Domain/Carts/Actions/UpdateCartLine.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Dystcz\LunarApi\Domain\Carts\Actions;
+
+use Dystcz\LunarApi\Domain\Carts\Data\CartLineData;
+use Dystcz\LunarApi\Domain\Carts\Models\Cart;
+use Dystcz\LunarApi\Domain\Carts\Models\CartLine;
+use JetBrains\PhpStorm\ArrayShape;
+use Lunar\Facades\CartSession;
+
+class UpdateCartLine
+{
+    #[ArrayShape([Cart::class, CartLine::class])]
+    public function __invoke(CartLineData $data, CartLine $cartLine): array
+    {
+        /** @var Cart $cart */
+        $cart = CartSession::manager();
+
+        $cart = $cart->updateLine(
+            cartLineId: $cartLine->id,
+            quantity: $data->quantity,
+            meta: $data->meta ?? []
+        );
+
+        $cartLine = $cart->lines->firstWhere(
+            fn (CartLine $cartLine) => $cartLine->is($cartLine)
+        );
+
+        return [$cart, $cartLine];
+    }
+}
