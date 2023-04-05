d5744ab363db083271a9c7d564b9d7cbb383f586
Jakub Theimer <jakub@dy.st>
2023-04-06 01:44:56 +0200

    add thumbnail relation for product variant

--- src/Domain/ProductVariants/Models/ProductVariant.php
+++ src/Domain/ProductVariants/Models/ProductVariant.php
@@ -11,8 +11,32 @@ use Lunar\Models\ProductVariant as LunarPoductVariant;
 class ProductVariant extends LunarPoductVariant
 {
     /**
+     * Create a new factory instance for the model.
+     */
+    protected static function newFactory(): ProductVariantFactory
+    {
+        return ProductVariantFactory::new();
+    }
+
+    /**
+     * Thumbnail relation.
+     */
+    public function thumbnail(): MorphOne
+    {
+        return $this->product->morphOne(config('media-library.media_model'), 'model')
+            ->where(function ($query) {
+                $query->where('id', function ($q) {
+                        return $q->from('dystore_media_product_variant')
+                            ->select('media_id')
+                            ->where('product_variant_id', $this->getKey())
+                            ->where('primary', true)
+                            ->take(1);
+                    });
+            });
+    }
+
+    /**
      * Lowest price relation.
-     *
      *
      * @throws InvalidArgumentException
      */
@@ -23,10 +47,5 @@ class ProductVariant extends LunarPoductVariant
                 LunarPrice::class,
                 'priceable'
             )->ofMany('price', 'min');
-    }
-
-    protected static function newFactory(): ProductVariantFactory
-    {
-        return ProductVariantFactory::new();
     }
 }
