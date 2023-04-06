c3e4a7de978e8222259a4bbf888d375b545b87b9
Jakub Theimer <jakub@dy.st>
2023-04-06 15:24:29 +0200

    add detach shipping option request

--- src/Domain/Carts/Http/Controllers/DetachShippingOptionController.php
+++ src/Domain/Carts/Http/Controllers/DetachShippingOptionController.php
@@ -5,14 +5,14 @@ namespace Dystcz\LunarApi\Domain\Carts\Http\Controllers;
 use Dystcz\LunarApi\Controller;
 use Dystcz\LunarApi\Domain\Carts\JsonApi\V1\CartAddressSchema;
 use Dystcz\LunarApi\Domain\Carts\Models\CartAddress;
-use Illuminate\Http\Request;
+use Dystcz\LunarApi\Domain\Shipping\JsonApi\V1\DetachShippingOptionRequest;
 use LaravelJsonApi\Core\Responses\DataResponse;
 
 class DetachShippingOptionController extends Controller
 {
     public function detachShippingOption(
         CartAddressSchema $schema,
-        Request $request,
+        DetachShippingOptionRequest $request,
         CartAddress $cartAddress,
     ): DataResponse {
         $this->authorize('update', $cartAddress);
