8ea0b1776e5b3f496d71cd6ce3e4268bff475971
Jakub Theimer <jakub@dy.st>
2023-04-06 14:13:40 +0200

    update factory

--- src/Domain/Carts/Factories/CartAddressFactory.php
+++ src/Domain/Carts/Factories/CartAddressFactory.php
@@ -12,9 +12,23 @@ class CartAddressFactory extends LunarCartAddressFactory
 
     public function definition(): array
     {
-        return array_merge(parent::definition(), [
+        return [
+            'title' => $this->faker->title,
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'company_name' => $this->faker->company,
+            'line_one' => $this->faker->streetName,
+            'line_two' => $this->faker->secondaryAddress,
+            'line_three' => $this->faker->buildingNumber,
+            'city' => $this->faker->city,
+            'state' => $this->faker->state,
+            'postcode' => $this->faker->postcode,
+            'delivery_instructions' => $this->faker->sentence,
             'contact_email' => $this->faker->safeEmail,
-            'country_id' => Country::first()->id ?? Country::factory(),
-        ]);
+            'contact_phone' => $this->faker->phoneNumber,
+            'type' => 'shipping',
+            'meta' => ['has_dog' => 'yes'],
+            'country_id' => Country::first() ?? Country::factory(),
+        ];
     }
 }
