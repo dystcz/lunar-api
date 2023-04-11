75d590505bba62f04040cc77371ba674631a0d32
Jakub Theimer <jakub@dy.st>
2023-04-11 15:33:38 +0200

    remove optional parameter

--- src/Domain/Prices/Actions/GetPrice.php
+++ src/Domain/Prices/Actions/GetPrice.php
@@ -3,7 +3,6 @@
 namespace Dystcz\LunarApi\Domain\Prices\Actions;
 
 use Illuminate\Support\Facades\Config;
-use Lunar\Base\Purchasable;
 use Lunar\DataTypes\Price;
 
 class GetPrice
@@ -22,10 +21,10 @@ class GetPrice
     /**
      * Get price with or withour tax based on config.
      */
-    public function __invoke(Price $price, ?Purchasable $purchasable = null): Price
+    public function __invoke(Price $price): Price
     {
         if ($this->withTax) {
-            return ($this->getPriceWithDefaultTax)($purchasable ?? $price->priceable, $price);
+            return ($this->getPriceWithDefaultTax)($price->priceable, $price);
         }
 
         return $price;
