9f0895b8ff00dacb2f64c74e7ec82cba83a5bd86
Jakub Theimer <jakub@dy.st>
2023-04-16 22:52:56 +0200

    add custom model;

---
+++ src/Domain/ProductsAssociations/Models/ProductAssociation.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dystcz\LunarApi\Domain\ProductsAssociations\Models;
+
+use Lunar\Models\ProductAssociation as LunarProductAssociation;
+
+class ProductAssociation extends LunarProductAssociation
+{
+}
