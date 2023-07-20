16781b3f7de7a33dc85486a457734871a920455e
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-07-21 01:31:08 +0200

    Use config facade

--- src/Domain/ProductVariants/Models/ProductVariant.php
+++ src/Domain/ProductVariants/Models/ProductVariant.php
@@ -6,6 +6,7 @@ use Dystcz\LunarApi\Domain\ProductVariants\Enums\PurchaseStatus;
 use Dystcz\LunarApi\Domain\ProductVariants\Factories\ProductVariantFactory;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Support\Facades\Config;
 use InvalidArgumentException;
 use Lunar\Models\Price as LunarPrice;
 use Lunar\Models\ProductVariant as LunarPoductVariant;
@@ -47,7 +48,7 @@ class ProductVariant extends LunarPoductVariant
         return $this->product->morphOne(config('media-library.media_model'), 'model')
         ->where(function ($query) {
             $query->where('id', function ($q) {
-                    $prefix = config('lunar.database.table_prefix');
+                    $prefix = Config::get('lunar.database.table_prefix');
 
                     return $q->from("{$prefix}media_product_variant")
                         ->select('media_id')
