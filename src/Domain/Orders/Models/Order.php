9f1667b8fcf9a7e8e2ef0dafcfc61c8293aad309
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-09-06 02:29:38 +0200

    remove custom cart relation

--- src/Domain/Orders/Models/Order.php
+++ src/Domain/Orders/Models/Order.php
@@ -2,13 +2,8 @@
 
 namespace Dystcz\LunarApi\Domain\Orders\Models;
 
-use Lunar\Models\Cart;
 use Lunar\Models\Order as LunarOrder;
 
 class Order extends LunarOrder
 {
-    public function cart(): \Illuminate\Database\Eloquent\Relations\HasOne
-    {
-        return $this->hasOne(Cart::class);
-    }
 }
