7d06d935a54f8e40929b3da17f94f423d4ecf48b
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-09-18 19:21:33 +0200

    add interacts with attributes trait

--- src/Domain/Customers/Models/Customer.php
+++ src/Domain/Customers/Models/Customer.php
@@ -2,11 +2,17 @@
 
 namespace Dystcz\LunarApi\Domain\Customers\Models;
 
+use Dystcz\LunarApi\Domain\Attributes\Traits\InteractsWithAttributes;
 use Dystcz\LunarApi\Domain\Customers\Factories\CustomerFactory;
 use Lunar\Models\Customer as LunarCustomer;
 
+/**
+ * @method HasMany attributes()
+ */
 class Customer extends LunarCustomer
 {
+    use InteractsWithAttributes;
+
     /**
      * Create a new factory instance for the model.
      */
