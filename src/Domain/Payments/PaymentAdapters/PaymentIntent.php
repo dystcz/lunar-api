e5e320236bc82cc5de2b230997f9d6e1494bab58
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-03 17:21:48 +0200

    add nullable client secret property

--- src/Domain/Payments/PaymentAdapters/PaymentIntent.php
+++ src/Domain/Payments/PaymentAdapters/PaymentIntent.php
@@ -6,13 +6,18 @@ class PaymentIntent
 {
     public function __construct(
         public readonly int|string $id,
+        public readonly ?string $client_secret = null,
     ) {
     }
 
+    /**
+     * @return array<string,mixed>
+     */
     public function toArray(): array
     {
         return [
             'id' => $this->id,
+            'client_secret' => $this->client_secret,
         ];
     }
 }
