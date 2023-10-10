f86885f0b364dc5b797c4a6e76e7a7b6a2fe811c
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-10 19:12:33 +0200

    style fix

--- src/Domain/Carts/JsonApi/V1/UpdateCartAddressCountryRequest.php
+++ src/Domain/Carts/JsonApi/V1/UpdateCartAddressCountryRequest.php
@@ -3,17 +3,26 @@
 namespace Dystcz\LunarApi\Domain\Carts\JsonApi\V1;
 
 use LaravelJsonApi\Laravel\Http\Requests\ResourceRequest;
+use LaravelJsonApi\Validation\Rule;
 
 class UpdateCartAddressCountryRequest extends ResourceRequest
 {
     /**
      * Get the validation rules for the resource.
+     *
+     * @return array<string,array>
      */
     public function rules(): array
     {
         return [
-            'cart' => [\LaravelJsonApi\Validation\Rule::toOne(), 'required'],
-            'country' => [\LaravelJsonApi\Validation\Rule::toOne(), 'required'],
+            'cart' => [
+                Rule::toOne(),
+                'required',
+            ],
+            'country' => [
+                Rule::toOne(),
+                'required',
+            ],
         ];
     }
 }
