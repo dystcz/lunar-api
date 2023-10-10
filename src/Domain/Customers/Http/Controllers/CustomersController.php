be6919a71ff3c370602446869edf3a7b18aa25a8
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-10 21:19:19 +0200

    cleanup

--- src/Domain/Customers/Http/Controllers/CustomersController.php
+++ src/Domain/Customers/Http/Controllers/CustomersController.php
@@ -3,7 +3,6 @@
 namespace Dystcz\LunarApi\Domain\Customers\Http\Controllers;
 
 use Dystcz\LunarApi\Controller;
-use LaravelJsonApi\Laravel\Http\Controllers\Actions;
 use LaravelJsonApi\Laravel\Http\Controllers\Actions\FetchOne;
 use LaravelJsonApi\Laravel\Http\Controllers\Actions\FetchRelated;
 use LaravelJsonApi\Laravel\Http\Controllers\Actions\FetchRelationship;
@@ -11,17 +10,8 @@ use LaravelJsonApi\Laravel\Http\Controllers\Actions\Update;
 
 class CustomersController extends Controller
 {
-    // use Actions\FetchMany;
     use FetchOne;
-
-    // use Actions\Store;
-    use Update;
-
-    // use Actions\Destroy;
     use FetchRelated;
     use FetchRelationship;
-
-    // use Actions\UpdateRelationship;
-    // use Actions\AttachRelationship;
-    // use Actions\DetachRelationship;
+    use Update;
 }
