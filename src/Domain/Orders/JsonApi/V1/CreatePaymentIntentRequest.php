d3d7229a4f01a5e67a87abf67914c9cab7097ae6
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-12 22:30:02 +0200

    remove hack

--- src/Domain/Orders/JsonApi/V1/CreatePaymentIntentRequest.php
+++ src/Domain/Orders/JsonApi/V1/CreatePaymentIntentRequest.php
@@ -2,37 +2,25 @@
 
 namespace Dystcz\LunarApi\Domain\Orders\JsonApi\V1;
 
-use Dystcz\LunarApi\Domain\JsonApi\Extensions\Schema\SchemaExtension;
-use Dystcz\LunarApi\Domain\JsonApi\Extensions\Schema\SchemaManifest;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Validation\Rule;
-use LaravelJsonApi\Eloquent\Fields\ArrayList;
-use LaravelJsonApi\Eloquent\Fields\Str;
 use LaravelJsonApi\Laravel\Http\Requests\ResourceRequest;
 
 class CreatePaymentIntentRequest extends ResourceRequest
 {
-    // Hack to add a field to the schema on the fly
-    protected function prepareForValidation(): void
-    {
-        /** @var SchemaExtension $orderSchemaExtension */
-        $orderSchemaExtension = SchemaManifest::for(OrderSchema::class);
-
-        $orderSchemaExtension->setFields([
-            Str::make('payment_method'),
-            ArrayList::make('meta'),
-        ]);
-    }
-
     /**
      * Get the validation rules for the resource.
+     *
+     * @return array<string,array>
      */
     public function rules(): array
     {
         $paymentTypes = Config::get('lunar.payments.types');
 
         return [
-            'meta' => 'array',
+            'meta' => [
+                'array',
+            ],
             'payment_method' => [
                 'required',
                 'string',
