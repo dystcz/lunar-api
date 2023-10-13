141cd0621cd352c1f9a39b02f7aecf7b96c7111b
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-13 16:12:35 +0200

    finish check order payment controller

--- src/Domain/Orders/Http/Controllers/CheckOrderPaymentStatusController.php
+++ src/Domain/Orders/Http/Controllers/CheckOrderPaymentStatusController.php
@@ -3,22 +3,41 @@
 namespace Dystcz\LunarApi\Domain\Orders\Http\Controllers;
 
 use Dystcz\LunarApi\Controller;
-use Dystcz\LunarApi\Domain\Orders\JsonApi\V1\CheckOrderPaymentStatusRequest;
+use Dystcz\LunarApi\Domain\Orders\JsonApi\V1\CheckOrderPaymentStatusQuery;
+use Dystcz\LunarApi\Domain\Orders\JsonApi\V1\OrderSchema;
 use Dystcz\LunarApi\Domain\Orders\Models\Order;
-use Dystcz\LunarApi\Domain\Payments\Actions\CheckOrderPaymentStatus;
 use LaravelJsonApi\Core\Responses\DataResponse;
 
 class CheckOrderPaymentStatusController extends Controller
 {
     public function checkOrderPaymentStatus(
-        // CheckOrderPaymentStatusRequest $request,
+        OrderSchema $schema,
+        CheckOrderPaymentStatusQuery $query,
         Order $order,
-        CheckOrderPaymentStatus $checkPaymentStatus,
     ): DataResponse {
         $this->authorize('viewSigned', $order);
 
-        $transaction = $checkPaymentStatus($order);
-
-        return DataResponse::make([]);
+        return DataResponse::make($order)
+            ->withIncludePaths([
+                'latest_transaction',
+            ])
+            ->withSparseFieldSets([
+                'orders' => [
+                    // 'latest_transaction',
+                    'status',
+                    'placed_at',
+                ],
+                'transactions' => [
+                    'type',
+                    'driver',
+                    'status',
+                    'success',
+                    'error',
+                ],
+            ])
+            ->withMeta([
+                'is_placed' => $order->isPlaced(),
+            ])
+            ->didntCreate();
     }
 }
