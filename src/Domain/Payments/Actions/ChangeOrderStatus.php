b1297b708faa5be5b12285ededf27905bf99803b
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-13 05:07:17 +0200

    add change order status action

---
+++ src/Domain/Payments/Actions/ChangeOrderStatus.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Dystcz\LunarApi\Domain\Payments\Actions;
+
+use Dystcz\LunarApi\Domain\Orders\Contracts\OrderStatusContract;
+use Dystcz\LunarApi\Domain\Orders\Events\OrderStatusChanged;
+use Lunar\Models\Order;
+
+class ChangeOrderStatus
+{
+    public function __construct(
+    ) {
+    }
+
+    /**
+     * Change order status to pending payment.
+     */
+    public function __invoke(Order $order, OrderStatusContract $orderStatus): Order
+    {
+        $order->update([
+            'status' => $orderStatus->value,
+        ]);
+
+        OrderStatusChanged::dispatch($order);
+
+        return $order;
+    }
+}
