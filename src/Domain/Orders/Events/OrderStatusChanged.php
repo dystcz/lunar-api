847f93fbf8d829a57d7bdca15bb277f9d3164fff
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-13 05:06:38 +0200

    add order status changed event

---
+++ src/Domain/Orders/Events/OrderStatusChanged.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Dystcz\LunarApi\Domain\Orders\Events;
+
+use Domain\Orders\Models\Order;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class OrderStatusChanged
+{
+    use Dispatchable;
+    use SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(public Order $order)
+    {
+    }
+}
