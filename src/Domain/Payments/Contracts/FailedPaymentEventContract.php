163cac3a38109b27a6dd764cefd4eaededb45557
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-13 16:13:37 +0200

    add failed payment event contract

---
+++ src/Domain/Payments/Contracts/FailedPaymentEventContract.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Dystcz\LunarApi\Domain\Payments\Contracts;
+
+use Dystcz\LunarApi\Domain\Payments\PaymentAdapters\PaymentAdapter;
+use Dystcz\LunarApi\Domain\Payments\PaymentAdapters\PaymentIntent;
+use Lunar\Models\Order;
+
+interface FailedPaymentEventContract
+{
+    /**
+     * Get payment adapter.
+     */
+    public function getPaymentAdapter(): PaymentAdapter;
+
+    /**
+     * Get order.
+     */
+    public function getOrder(): Order;
+
+    /**
+     * Get payment intent.
+     */
+    public function getPaymentIntent(): PaymentIntent;
+}
