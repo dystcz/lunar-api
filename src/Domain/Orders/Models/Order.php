b9f853c06e744567b799f89e6dfbd12407ab2812
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-13 16:13:16 +0200

    add latest transaction relation

--- src/Domain/Orders/Models/Order.php
+++ src/Domain/Orders/Models/Order.php
@@ -2,8 +2,19 @@
 
 namespace Dystcz\LunarApi\Domain\Orders\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Lunar\Models\Order as LunarOrder;
+use Lunar\Models\Transaction;
 
 class Order extends LunarOrder
 {
+    /**
+     * Get the latest transaction for the order.
+     */
+    public function latestTransaction(): HasOne
+    {
+        return $this
+            ->hasOne(Transaction::class)
+            ->latestOfMany();
+    }
 }
