b0635dd0c17a5ab0fc58972070c2290ce288e79c
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-13 05:06:33 +0200

    implement contract

--- src/Domain/Orders/Enums/OrderStatus.php
+++ src/Domain/Orders/Enums/OrderStatus.php
@@ -2,11 +2,12 @@
 
 namespace Dystcz\LunarApi\Domain\Orders\Enums;
 
-enum OrderStatus: string
+use Dystcz\LunarApi\Domain\Orders\Contracts\OrderStatusContract;
+
+enum OrderStatus: string implements OrderStatusContract
 {
     case AWAITING_PAYMENT = 'awaiting-payment';
     case PENDING_PAYMENT = 'pending-payment';
-    case BANK_PAYMENT_RECEIVED = 'bank-payment-received';
     case PAYMENT_RECEIVED = 'payment-received';
     case MANUFACTURING = 'manufacturing';
     case DISPATCHED = 'dispatched';
