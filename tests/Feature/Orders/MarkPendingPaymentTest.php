6ef98910e614b83f6a57beff01ea7b6c869d71e2
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-13 05:07:01 +0200

    add mark pending payment test

---
+++ tests/Feature/Orders/MarkPendingPaymentTest.php
@@ -0,0 +1,49 @@
+<?php
+
+use Dystcz\LunarApi\Domain\Carts\Events\CartCreated;
+use Dystcz\LunarApi\Domain\Carts\Models\Cart;
+use Dystcz\LunarApi\Domain\Orders\Enums\OrderStatus;
+use Dystcz\LunarApi\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\URL;
+
+uses(TestCase::class, RefreshDatabase::class);
+
+beforeEach(function () {
+    /** @var TestCase $this */
+    Event::fake(CartCreated::class);
+
+    /** @var Cart $cart */
+    $cart = Cart::factory()
+        ->withAddresses()
+        ->withLines()
+        ->create();
+
+    $this->order = $cart->createOrder();
+});
+
+test('can change order status to pending payment', function () {
+    /** @var TestCase $this */
+    $url = URL::signedRoute(
+        'v1.orders.markPendingPayment', ['order' => $this->order->id]
+    );
+
+    $response = $this
+        ->jsonApi()
+        ->expects('orders')
+        ->withData([
+            'type' => 'orders',
+            'id' => (string) $this->order->getRouteKey(),
+        ])
+        ->patch($url);
+
+    $response->assertSuccessful();
+
+    $id = $response->getId();
+
+    $this->assertDatabaseHas($this->order->getTable(), [
+        'id' => $id,
+        'status' => OrderStatus::PENDING_PAYMENT,
+    ]);
+});
