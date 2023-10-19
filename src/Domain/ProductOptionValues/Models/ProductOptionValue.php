50b6bf3b12356cc7d02e1c2c4c81f7f9eb391871
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-19 03:33:33 +0200

    remove unused class

--- src/Domain/ProductOptionValues/Models/ProductOptionValue.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Dystcz\LunarApi\Domain\ProductOptionValues\Models;
-
-use Lunar\Models\ProductOptionValue as LunarProductOptionValue;
-
-class ProductOptionValue extends LunarProductOptionValue
-{
-}
