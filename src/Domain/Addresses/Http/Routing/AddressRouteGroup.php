3927efc3b371708b2b0ececd9c2c060e6fd9bbd5
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-19 03:31:04 +0200

    style fix

--- src/Domain/Addresses/Http/Routing/AddressRouteGroup.php
+++ src/Domain/Addresses/Http/Routing/AddressRouteGroup.php
@@ -13,7 +13,7 @@ class AddressRouteGroup extends RouteGroup
     /**
      * Register routes.
      */
-    public function routes(?string $prefix = null, array|string $middleware = []): void
+    public function routes(string $prefix = null, array|string $middleware = []): void
     {
         JsonApiRoute::server('v1')
             ->prefix('v1')
