92ea5b6f83a6c0712ad4285225eb463ffeaf87be
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-20 02:57:53 +0200

    add uses hashids method

--- src/LunarApi.php
+++ src/LunarApi.php
@@ -5,16 +5,31 @@ namespace Dystcz\LunarApi;
 use Dystcz\LunarApi\Domain\Users\Contracts\CreatesUserFromCart;
 use Dystcz\LunarApi\Domain\Users\Contracts\RegistersUser;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
 
 class LunarApi
 {
+    /**
+     * Create user from cart using callback.
+     */
     public static function createUserFromCartUsing(string $callback): void
     {
         App::singleton(CreatesUserFromCart::class, $callback);
     }
 
+    /**
+     * Register user using callback.
+     */
     public static function registerUserUsing(string $callback): void
     {
         App::singleton(RegistersUser::class, $callback);
+    }
+
+    /**
+     * Check if hashids are used.
+     */
+    public static function usesHashids(): bool
+    {
+        return Config::get('lunar-api.schemas.use_hashids', false);
     }
 }
