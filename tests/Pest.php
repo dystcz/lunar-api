4d377cfb38dc6aaf5f9635aa57fe947ffc64ae36
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-23 15:39:28 +0200

    fix method

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Dystcz\LunarApi\Support\Models\Actions\GetModelKey;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Config;
 use Lunar\Base\BaseModel;
@@ -16,5 +17,5 @@ function decodeHashedId(BaseModel $model, mixed $id): mixed
     /** @var \Vinkla\Hashids\Facades\Hashids $hashids */
     $hashids = App::get('hashids');
 
-    return $hashids->connection($model->getMorphClass())->decode($id);
+    return $hashids->connection((new GetModelKey)($model))->decode($id);
 }
