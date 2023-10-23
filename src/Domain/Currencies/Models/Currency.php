3ea746f187ca9c3562982c9da364e77751af5708
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-23 15:38:49 +0200

    add route key hashing trait'

--- src/Domain/Currencies/Models/Currency.php
+++ src/Domain/Currencies/Models/Currency.php
@@ -3,10 +3,13 @@
 namespace Dystcz\LunarApi\Domain\Currencies\Models;
 
 use Dystcz\LunarApi\Domain\Currencies\Factories\CurrencyFactory;
+use Dystcz\LunarApi\Hashids\Traits\HashesRouteKey;
 use Lunar\Models\Currency as LunarCurrency;
 
 class Currency extends LunarCurrency
 {
+    use HashesRouteKey;
+
     /**
      * Create a new factory instance for the model.
      */
