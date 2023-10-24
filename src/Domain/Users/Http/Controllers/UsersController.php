d8269d8e848fc02b962bc2763ca19bc5e6b651e1
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-24 20:40:00 +0200

    style fix

--- src/Domain/Users/Http/Controllers/UsersController.php
+++ src/Domain/Users/Http/Controllers/UsersController.php
@@ -3,22 +3,11 @@
 namespace Dystcz\LunarApi\Domain\Users\Http\Controllers;
 
 use Dystcz\LunarApi\Controller;
-use LaravelJsonApi\Laravel\Http\Controllers\Actions;
 use LaravelJsonApi\Laravel\Http\Controllers\Actions\FetchRelated;
 use LaravelJsonApi\Laravel\Http\Controllers\Actions\FetchRelationship;
 
 class UsersController extends Controller
 {
-    // use Actions\FetchMany;
-    // use Actions\FetchOne;
-
-    // use Actions\Store;
-    // use Actions\Update;
-    // use Actions\Destroy;
     use FetchRelated;
     use FetchRelationship;
-
-    // use Actions\UpdateRelationship;
-    // use Actions\AttachRelationship;
-    // use Actions\DetachRelationship;
 }
