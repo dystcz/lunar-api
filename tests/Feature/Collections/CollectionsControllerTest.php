6a398cbfe3d4f34d732db5dc6d9edec3b2c10964
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-31 21:22:55 +0100

    update tests

--- tests/Feature/Collections/CollectionsControllerTest.php
+++ tests/Feature/Collections/CollectionsControllerTest.php
@@ -2,12 +2,16 @@
 
 use Dystcz\LunarApi\Domain\CollectionGroups\Models\CollectionGroup;
 use Dystcz\LunarApi\Domain\Collections\Models\Collection;
+use Dystcz\LunarApi\Domain\Media\Factories\MediaFactory;
 use Dystcz\LunarApi\Domain\Prices\Factories\PriceFactory;
-use Dystcz\LunarApi\Domain\Products\Factories\ProductFactory;
+use Dystcz\LunarApi\Domain\Prices\Models\Price;
 use Dystcz\LunarApi\Domain\Products\Models\Product;
 use Dystcz\LunarApi\Domain\ProductVariants\Factories\ProductVariantFactory;
+use Dystcz\LunarApi\Domain\ProductVariants\Models\ProductVariant;
+use Dystcz\LunarApi\Tests\Data\TestInclude;
 use Dystcz\LunarApi\Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Collection as IlluminateCollection;
 
 uses(TestCase::class, RefreshDatabase::class);
 
@@ -95,6 +99,46 @@ it('can list collections with included default url', function () {
 
 })->group('collections');
 
+it('can list collections with included products', function () {
+
+    /** @var TestCase $this */
+    $includes = collect([
+        'products' => new TestInclude(
+            type: 'products',
+            relation: 'products',
+        ),
+        'products.prices' => new TestInclude(
+            type: 'prices',
+            relation: 'prices',
+            relationCallback: fn (IlluminateCollection $models) => $models->pluck('products')->flatten()->pluck('prices')->flatten(),
+        ),
+        'products.lowest_price' => new TestInclude(
+            type: 'prices',
+            relation: 'lowestPrice',
+            relationCallback: fn (IlluminateCollection $models) => $models->pluck('products')->flatten()->pluck('lowestPrice'),
+        ),
+        'products.thumbnail' => new TestInclude(
+            type: 'media',
+            relation: 'thumbnail',
+            relationCallback: fn (IlluminateCollection $models) => $models->pluck('products')->flatten()->pluck('thumbnail'),
+        ),
+        'products.images' => new TestInclude(
+            type: 'media',
+            factory: Product::factory()
+                ->has(MediaFactory::new()->thumbnail(), 'thumbnail')
+                ->has(MediaFactory::new()->count(2), 'images')
+                ->has(ProductVariant::factory()->has(Price::factory(), 'prices')->count(2), 'variants')
+                ->count(3),
+            factory_relation: 'products',
+            relation: 'images',
+            relationCallback: fn (IlluminateCollection $models) => $models->pluck('products')->flatten()->pluck('images')->flatten(),
+        ),
+    ]);
+
+    $response = $this->testIndexWithIncludes('collections', Collection::class, 5, $includes);
+
+})->group('collections');
+
 it('can read collection detail', function () {
     /** @var TestCase $this */
     $model = Collection::factory()->create();
@@ -114,11 +158,11 @@ it('can read products in a collection', function () {
     /** @var TestCase $this */
     $model = Collection::factory()
         ->has(
-            ProductFactory::new()
+            Product::factory()
                 ->has(
-                    ProductVariantFactory::new()
+                    ProductVariant::factory()
                         ->has(
-                            PriceFactory::new()
+                            Price::factory()
                         )
                         ->count(2),
                     'variants'
