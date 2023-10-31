40d377fdb401681091f067e699ffdb6d01a4c3b0
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-31 15:55:08 +0100

    add helper methods

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,10 +1,15 @@
 <?php
 
 use Dystcz\LunarApi\Support\Models\Actions\GetModelKey;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Config;
 use Lunar\Base\BaseModel;
+use Lunar\Database\Factories\LanguageFactory;
 
+/**
+ * Get server url.
+ */
 function serverUrl(string $path = null, bool $full = false): string
 {
     $path = implode('/', [Config::get('lunar-api.general.route_prefix'), 'v1', ltrim($path, '/')]);
@@ -12,10 +17,35 @@ function serverUrl(string $path = null, bool $full = false): string
     return $path;
 }
 
+/**
+ * Decode hashed id.
+ */
 function decodeHashedId(BaseModel $model, mixed $id): mixed
 {
     /** @var \Vinkla\Hashids\Facades\Hashids $hashids */
     $hashids = App::get('hashids');
 
     return $hashids->connection((new GetModelKey)($model))->decode($id);
+}
+
+/**
+ * Map models to response data.
+ */
+function mapModelsToResponseData(string $type, Collection $models): Collection
+{
+    return $models->map(fn ($model) => [
+        'type' => $type,
+        'id' => $model->getRouteKey(),
+    ]);
+}
+
+/**
+ * Turn on url generator.
+ */
+function generateUrls(): void
+{
+    LanguageFactory::new()->create();
+
+    /** @var TestCase $this */
+    Config::set('lunar.urls.generator', \Lunar\Generators\UrlGenerator::class);
 }
