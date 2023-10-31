2a85a952df0f4a2ef9664ef58be6c5fe3e9a8e0b
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-31 05:44:06 +0100

    refactor registering routes

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,13 @@
 <?php
 
 use Dystcz\LunarApi\Domain\Payments\Http\Controllers\HandlePaymentWebhookController;
-use Dystcz\LunarApi\Routing\Contracts\RouteGroup;
-use Illuminate\Support\Collection;
+use Dystcz\LunarApi\Support\Config\Actions\RegisterRoutesFromConfig;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
-    'prefix' => Config::get('lunar-api.route_prefix'),
-    'middleware' => Config::get('lunar-api.route_middleware'),
-], function () {
-    $domains = Collection::make(Config::get('lunar-api.domains'));
-
-    foreach ($domains as $domain) {
-        if (isset($domain['route_groups'])) {
-            foreach ($domain['route_groups'] as $group) {
-                /** @var RouteGroup $group */
-                $group::make($domain['schema']::type())->routes();
-            }
-        }
-    }
-});
+    'prefix' => Config::get('lunar-api.general.route_prefix'),
+    'middleware' => Config::get('lunar-api.general.route_middleware'),
+], fn () => RegisterRoutesFromConfig::run());
 
 Route::post('{paymentDriver}/webhook', HandlePaymentWebhookController::class);
