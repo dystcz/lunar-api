e55fd641f68e220127340731a4eeb78b74008d60
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-31 08:11:08 +0100

    add possibility to load domains from config by its key

--- src/Support/Config/Actions/RegisterRoutesFromConfig.php
+++ src/Support/Config/Actions/RegisterRoutesFromConfig.php
@@ -7,9 +7,9 @@ use Dystcz\LunarApi\Support\Config\Collections\DomainConfigCollection;
 
 class RegisterRoutesFromConfig extends Action
 {
-    public function handle(): void
+    public function handle(string $configKey = null): void
     {
-        $domains = DomainConfigCollection::make()
+        DomainConfigCollection::fromConfig($configKey ?? 'lunar-api.domains')
             ->getRoutes()
             ->each(
                 fn (string $routeGroup, string $schemaType) => $routeGroup::make($schemaType)->routes(),
