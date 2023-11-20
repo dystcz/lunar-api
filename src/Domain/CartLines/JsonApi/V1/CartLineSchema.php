46b6f2430b0d69981dee5d1a9ef7b038a8aed63c
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-11-20 16:39:55 +0000

    add prices map

--- src/Domain/CartLines/JsonApi/V1/CartLineSchema.php
+++ src/Domain/CartLines/JsonApi/V1/CartLineSchema.php
@@ -4,6 +4,7 @@ namespace Dystcz\LunarApi\Domain\CartLines\JsonApi\V1;
 
 use Dystcz\LunarApi\Domain\JsonApi\Eloquent\Schema;
 use LaravelJsonApi\Eloquent\Fields\ArrayHash;
+use LaravelJsonApi\Eloquent\Fields\Map;
 use LaravelJsonApi\Eloquent\Fields\Number;
 use LaravelJsonApi\Eloquent\Fields\Relations\BelongsTo;
 use LaravelJsonApi\Eloquent\Fields\Relations\MorphTo;
@@ -25,12 +26,44 @@ class CartLineSchema extends Schema
         return [
             $this->idField(),
 
+            Map::make('prices', [
+                Number::make('unit_price', 'unitPrice')
+                    ->serializeUsing(
+                        static fn ($value) => $value?->decimal,
+                    ),
+                Number::make('quantity', 'quantity'),
+                Number::make('sub_total', 'subTotal')
+                    ->serializeUsing(
+                        static fn ($value) => $value?->decimal,
+                    ),
+                Number::make('sub_total_discounted', 'subTotalDiscounted')
+                    ->serializeUsing(
+                        static fn ($value) => $value?->decimal,
+                    ),
+                Number::make('total', 'total')
+                    ->serializeUsing(
+                        static fn ($value) => $value?->decimal,
+                    ),
+                Number::make('tax_amount', 'taxAmount')
+                    ->serializeUsing(
+                        static fn ($value) => $value?->decimal,
+                    ),
+                Number::make('discount_total', 'discounTotal')
+                    ->serializeUsing(
+                        static fn ($value) => $value?->decimal,
+                    ),
+            ]),
+
             Number::make('quantity'),
+
             ArrayHash::make('meta'),
+
             Number::make('purchasable_id'),
+
             Str::make('purchasable_type'),
 
             BelongsTo::make('cart'),
+
             MorphTo::make('purchasable', 'purchasable')
                 ->types('products', 'variants'),
 
