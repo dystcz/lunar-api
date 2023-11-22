15b33cda2317048599e3caa61d184850640e6f99
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-11-22 21:48:45 +0000

    add fetch one trait

--- src/Domain/ProductVariants/Http/Controllers/ProductVariantsController.php
+++ src/Domain/ProductVariants/Http/Controllers/ProductVariantsController.php
@@ -4,12 +4,14 @@ namespace Dystcz\LunarApi\Domain\ProductVariants\Http\Controllers;
 
 use Dystcz\LunarApi\Base\Controller;
 use LaravelJsonApi\Laravel\Http\Controllers\Actions\FetchMany;
+use LaravelJsonApi\Laravel\Http\Controllers\Actions\FetchOne;
 use LaravelJsonApi\Laravel\Http\Controllers\Actions\FetchRelated;
 use LaravelJsonApi\Laravel\Http\Controllers\Actions\FetchRelationship;
 
 class ProductVariantsController extends Controller
 {
     use FetchMany;
+    use FetchOne;
     use FetchRelated;
     use FetchRelationship;
 }
