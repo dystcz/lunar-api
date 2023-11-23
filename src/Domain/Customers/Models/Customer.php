9d1a38af781aa03c7ca37302413226f1e3de8e30
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-11-23 14:50:15 +0000

    add name and full name attributes to customer model

--- src/Domain/Customers/Models/Customer.php
+++ src/Domain/Customers/Models/Customer.php
@@ -5,6 +5,7 @@ namespace Dystcz\LunarApi\Domain\Customers\Models;
 use Dystcz\LunarApi\Domain\Attributes\Traits\InteractsWithAttributes;
 use Dystcz\LunarApi\Domain\Customers\Factories\CustomerFactory;
 use Dystcz\LunarApi\Hashids\Traits\HashesRouteKey;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Lunar\Models\Customer as LunarCustomer;
 
 /**
@@ -21,5 +22,23 @@ class Customer extends LunarCustomer
     protected static function newFactory(): CustomerFactory
     {
         return CustomerFactory::new();
+    }
+
+    /**
+     * Get name attribute.
+     */
+    protected function name(): Attribute
+    {
+        return $this->fullName();
+    }
+
+    /**
+     * Get full name attribute.
+     */
+    protected function fullName(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => "{$this->first_name} {$this->last_name}",
+        );
     }
 }
