109cd5e326faa85b1757741681549a50bd0cb4d8
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-11-28 15:18:30 +0000

    add order status change test

--- tests/Feature/Orders/OrderStatusChangeTest.php
+++ tests/Feature/Orders/OrderStatusChangeTest.php
@@ -9,7 +9,6 @@ use Dystcz\LunarApi\Domain\Orders\Models\Order;
 use Dystcz\LunarApi\Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Event;
-use Illuminate\Support\Facades\URL;
 
 uses(TestCase::class, RefreshDatabase::class);
 
@@ -28,36 +27,14 @@ beforeEach(function () {
     $this->order = Order::query()->where($order->getKeyName(), $order->getKey())->firstOrFail();
 });
 
-it('can change order status to pending payment', function () {
+it('sends order status changed event after order status was changed', function () {
     /** @var TestCase $this */
     Event::fake(OrderStatusChanged::class);
 
-    $url = URL::signedRoute(
-        'v1.orders.markAwaitingPayment', ['order' => $this->order->getRouteKey()]
-    );
-
     $this->order->update(['status' => OrderStatus::PENDING_PAYMENT->value]);
-
-    $response = $this
-        ->jsonApi()
-        ->expects('orders')
-        ->withData([
-            'type' => 'orders',
-            'id' => (string) $this->order->getRouteKey(),
-        ])
-        ->patch($url);
-
-    $response->assertSuccessful();
-
-    $id = $response->getId();
-
-    $this->assertDatabaseHas($this->order->getTable(), [
-        'id' => $this->order->getKey(),
-        'status' => OrderStatus::AWAITING_PAYMENT->value,
-    ]);
 
     Event::assertDispatched(
         OrderStatusChanged::class,
         fn (OrderStatusChanged $event) => $event->order->getKey() === $this->order->getKey(),
     );
-});
+})->group('orders');
