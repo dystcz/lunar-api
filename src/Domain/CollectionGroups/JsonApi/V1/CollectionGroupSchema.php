3c3683e5eadf7419475f777e20fc41606a9eaa1b
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-01-25 03:54:07 +0100

    add filters

--- src/Domain/CollectionGroups/JsonApi/V1/CollectionGroupSchema.php
+++ src/Domain/CollectionGroups/JsonApi/V1/CollectionGroupSchema.php
@@ -4,6 +4,8 @@ namespace Dystcz\LunarApi\Domain\CollectionGroups\JsonApi\V1;
 
 use Dystcz\LunarApi\Domain\JsonApi\Eloquent\Schema;
 use LaravelJsonApi\Eloquent\Fields\Str;
+use LaravelJsonApi\Eloquent\Filters\Where;
+use LaravelJsonApi\Eloquent\Filters\WhereIdIn;
 use Lunar\Models\CollectionGroup;
 
 class CollectionGroupSchema extends Schema
@@ -25,9 +27,23 @@ class CollectionGroupSchema extends Schema
 
             Str::make('handle'),
 
-            Str::make('value'),
-
             ...parent::fields(),
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function filters(): array
+    {
+        return [
+            WhereIdIn::make($this),
+
+            Where::make('name', 'name'),
+
+            Where::make('handle', 'handle'),
+
+            ...parent::filters(),
         ];
     }
 
