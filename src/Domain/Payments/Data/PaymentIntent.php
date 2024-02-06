0f9412cc0a64c657afa957249e69851fec5e0771
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-02-06 01:33:52 +0100

    update payment intent, implement contract

--- src/Domain/Payments/Data/PaymentIntent.php
+++ src/Domain/Payments/Data/PaymentIntent.php
@@ -2,19 +2,60 @@
 
 namespace Dystcz\LunarApi\Domain\Payments\Data;
 
-class PaymentIntent
+use Dystcz\LunarApi\Domain\Payments\Contracts\PaymentIntent as PaymentIntentContract;
+use Dystcz\LunarApi\Domain\Payments\Enums\PaymentIntentStatus;
+
+class PaymentIntent implements PaymentIntentContract
 {
+    /**
+     * @param  array<string,mixed>  $meta
+     */
     public function __construct(
-        public readonly mixed $paymentIntent,
+        public readonly mixed $intent,
+        public array $meta = [],
     ) {
     }
 
     /**
-     * Get payment intent ID.
+     * Get ID.
      */
     public function getId(): mixed
     {
-        return $this->paymentIntent->id;
+        return $this->intent->id;
+    }
+
+    /**
+     * Get amount.
+     */
+    public function getAmount(): int
+    {
+        return $this->intent->amount;
+    }
+
+    /**
+     * Get status.
+     */
+    public function getStatus(): string
+    {
+        return PaymentIntentStatus::INTENT->value;
+    }
+
+    /**
+     * Get client secret.
+     */
+    public function getClientSecret(): string
+    {
+        return $this->intent->client_secret;
+    }
+
+    /**
+     * Get meta.
+     *
+     * @return array<string,mixed>
+     */
+    public function getMeta(): array
+    {
+        return $this->meta;
     }
 
     /**
@@ -24,6 +65,10 @@ class PaymentIntent
     {
         return [
             'id' => $this->getId(),
+            'amount' => $this->getAmount(),
+            'status' => $this->getStatus(),
+            'client_secret' => $this->getClientSecret(),
+            'meta' => $this->getMeta(),
         ];
     }
 }
