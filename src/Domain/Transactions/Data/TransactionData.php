f5c719a4ca5713ee3ea5bacb97751fd91700197d
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-02-09 11:54:22 +0100

    add methods, rearrange properties

--- src/Domain/Transactions/Data/TransactionData.php
+++ src/Domain/Transactions/Data/TransactionData.php
@@ -15,43 +15,20 @@ class TransactionData implements Arrayable
      */
     public function __construct(
         public int $order_id,
-        public bool $success,
         public string $type,
         public string $driver,
         public float $amount,
         public string $reference,
         public string $status,
         public string $card_type,
+        public bool $success = false,
         public ?string $notes = null,
         public ?int $parent_transaction_id = null,
         public ?string $last_four = null,
         public ?Carbon $captured_at = null,
         public array $meta = [],
     ) {
-
         //
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function toArray(): array
-    {
-        return [
-            'parent_transaction_id' => $this->parent_transaction_id,
-            'order_id' => $this->order_id,
-            'success' => $this->success,
-            'type' => $this->type,
-            'driver' => $this->driver,
-            'amount' => $this->amount,
-            'reference' => $this->reference,
-            'status' => $this->status,
-            'card_type' => $this->card_type,
-            'notes' => $this->notes,
-            'last_four' => $this->last_four,
-            'captured_at' => $this->captured_at?->toDateTimeString(),
-            'meta' => $this->meta,
-        ];
     }
 
     /**
@@ -64,6 +41,42 @@ class TransactionData implements Arrayable
         $data = array_merge($this->toArray(), $data);
 
         return new static(...$data);
+    }
+
+    /**
+     * Set parent transaction id.
+     */
+    public function setParentId(int $parentId): self
+    {
+        $this->parent_transaction_id = $parentId;
+
+        return $this;
+    }
+
+    /**
+     * Set successfull.
+     */
+    public function setSuccessful(): self
+    {
+        return $this->setSuccess(true);
+    }
+
+    /**
+     * Set unsuccessfull.
+     */
+    public function setUnsuccessful(): self
+    {
+        return $this->setSuccess(false);
+    }
+
+    /**
+     * Set success.
+     */
+    public function setSuccess(bool $success): self
+    {
+        $this->success = $success;
+
+        return $this;
     }
 
     /**
@@ -94,5 +107,27 @@ class TransactionData implements Arrayable
     public function getError(): ?string
     {
         return $this->meta['error'] ?? null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function toArray(): array
+    {
+        return [
+            'parent_transaction_id' => $this->parent_transaction_id,
+            'order_id' => $this->order_id,
+            'success' => $this->success,
+            'type' => $this->type,
+            'driver' => $this->driver,
+            'amount' => $this->amount,
+            'reference' => $this->reference,
+            'status' => $this->status,
+            'card_type' => $this->card_type,
+            'notes' => $this->notes,
+            'last_four' => $this->last_four,
+            'captured_at' => $this->captured_at?->toDateTimeString(),
+            'meta' => $this->meta,
+        ];
     }
 }
