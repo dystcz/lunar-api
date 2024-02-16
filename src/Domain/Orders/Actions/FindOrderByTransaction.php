5a3919d9c3f54c4023fdd88c7c454bc5bc09de16
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-02-16 20:21:28 +0100

    add find order by transaction action

---
+++ src/Domain/Orders/Actions/FindOrderByTransaction.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Dystcz\LunarApi\Domain\Orders\Actions;
+
+use Dystcz\LunarApi\Domain\Orders\Models\Order;
+use Dystcz\LunarApi\Domain\Payments\Data\PaymentIntent;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class FindOrderByTransaction
+{
+    /**
+     * Find order by intent.
+     *
+     * @throws ModelNotFoundException
+     */
+    public function __invoke(PaymentIntent $intent): ?Order
+    {
+        return Order::query()
+            ->whereHas(
+                'transactions',
+                fn ($query) => $query->where('reference', $intent->getId()),
+            )
+            ->first();
+    }
+}
