9102e96d409cf3626230147f511af477234bd760
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-03-08 05:52:54 +0100

    add price tests

---
+++ tests/Feature/Domain/Prices/JsonApi/V1/DeletePriceTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Dystcz\LunarApi\Domain\Customers\Models\Customer;
+use Dystcz\LunarApi\Domain\Prices\Models\Price;
+use Dystcz\LunarApi\Domain\Products\Models\Product;
+use Dystcz\LunarApi\Tests\Stubs\Users\User;
+use Dystcz\LunarApi\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(TestCase::class, RefreshDatabase::class);
+
+beforeEach(function () {
+    /** @var TestCase $this */
+    $this->user = User::factory()
+        ->has(Customer::factory())
+        ->create();
+});
+
+test('prices cannot be deleted', function () {
+    /** @var TestCase $this */
+    $price = Price::factory()->for(Product::factory(), 'priceable')->create();
+
+    $response = $this->deleteTest('prices', $price);
+
+    $response->assertErrorStatus([
+        'status' => '404',
+        'title' => 'Not Found',
+    ]);
+})->group('prices', 'policies');
---
+++ tests/Feature/Domain/Prices/JsonApi/V1/UpdatePriceTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Dystcz\LunarApi\Domain\Customers\Models\Customer;
+use Dystcz\LunarApi\Domain\Products\Models\Product;
+use Dystcz\LunarApi\Tests\Stubs\Users\User;
+use Dystcz\LunarApi\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(TestCase::class, RefreshDatabase::class);
+
+beforeEach(function () {
+    /** @var TestCase $this */
+    $this->user = User::factory()
+        ->has(Customer::factory())
+        ->create();
+});
+
+test('prices cannot be updated', function () {
+    /** @var TestCase $this */
+    $response = $this->updateTest('prices', Product::class, []);
+
+    $response->assertErrorStatus([
+        'status' => '404',
+        'title' => 'Not Found',
+    ]);
+})->group('prices', 'policies');
