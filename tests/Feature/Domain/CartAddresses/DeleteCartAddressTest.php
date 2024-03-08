a94d8fe2c67456a3027f9b0e7555d7ff892fcd54
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-03-08 04:25:10 +0100

    add tests

---
+++ tests/Feature/Domain/CartAddresses/DeleteCartAddressTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Dystcz\LunarApi\Domain\CartAddresses\Models\CartAddress;
+use Dystcz\LunarApi\Domain\Carts\Models\Cart;
+use Dystcz\LunarApi\Domain\Customers\Models\Customer;
+use Dystcz\LunarApi\Tests\Stubs\Users\User;
+use Dystcz\LunarApi\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(TestCase::class, RefreshDatabase::class);
+
+beforeEach(function () {
+    /** @var TestCase $this */
+    $this->user = User::factory()
+        ->has(Customer::factory())
+        ->create();
+
+    $this->cart = Cart::factory()->create();
+
+    $this->cartAddress = CartAddress::factory()->for($this->cart)->create();
+});
+
+test('cart addresses cannot be deleted', function () {
+    /** @var TestCase $this */
+    $response = $this->deleteTest('cart-addresses', $this->cartAddress);
+
+    $response->assertErrorStatus([
+        'status' => '405',
+        'title' => 'Method Not Allowed',
+    ]);
+})->group('cart-addresses', 'policies');
