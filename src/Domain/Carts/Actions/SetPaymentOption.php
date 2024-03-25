23298c68a70e164f80a2131aac444bce9046504b
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-03-25 21:57:17 +0100

    fix action

--- src/Domain/Carts/Actions/SetPaymentOption.php
+++ src/Domain/Carts/Actions/SetPaymentOption.php
@@ -2,24 +2,20 @@
 
 namespace Dystcz\LunarApi\Domain\Carts\Actions;
 
+use Dystcz\LunarApi\Domain\Carts\Models\Cart;
 use Dystcz\LunarApi\Domain\PaymentOptions\Data\PaymentOption;
 use Lunar\Actions\AbstractAction;
-use Lunar\DataTypes\ShippingOption;
-use Lunar\Models\Cart;
-use Lunar\Models\CartLine;
 
 class SetPaymentOption extends AbstractAction
 {
     /**
      * Execute the action.
-     *
-     * @param  CartLine  $cartLine
-     * @param  ShippingOption  $customerGroups
      */
     public function execute(
         Cart $cart,
         PaymentOption $paymentOption
     ): self {
+        $cart->paymentOption = $paymentOption;
         $cart->update([
             'payment_option' => $paymentOption->getIdentifier(),
         ]);
