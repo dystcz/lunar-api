cd5d8b120293b384b32084d742c5fb83e838433c
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-03-26 00:59:05 +0100

    extract payment option logic and properties to a trait

--- src/Domain/Carts/Models/Cart.php
+++ src/Domain/Carts/Models/Cart.php
@@ -2,15 +2,12 @@
 
 namespace Dystcz\LunarApi\Domain\Carts\Models;
 
-use Dystcz\LunarApi\Domain\Carts\Actions\SetPaymentOption;
 use Dystcz\LunarApi\Domain\Carts\Events\CartCreated;
 use Dystcz\LunarApi\Domain\Carts\Factories\CartFactory;
+use Dystcz\LunarApi\Domain\Carts\Traits\InteractsWithPaymentOptions;
 use Dystcz\LunarApi\Domain\Carts\ValueObjects\PaymentBreakdown;
 use Dystcz\LunarApi\Domain\PaymentOptions\Data\PaymentOption;
-use Dystcz\LunarApi\Domain\PaymentOptions\Facades\PaymentManifest;
 use Dystcz\LunarApi\Hashids\Traits\HashesRouteKey;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Config;
 use Lunar\DataTypes\Price;
 use Lunar\Models\Cart as LunarCart;
 
@@ -21,10 +18,15 @@ use Lunar\Models\Cart as LunarCart;
  * @property PaymentOption|null $paymentOptionOverride
  * @property array $paymentEstimateMeta
  * @property PaymentBreakdown|null $paymentBreakdown
+ *
+ * @method Cart setPaymentOption(PaymentOption $option, bool $refresh = true)
+ * @method Cart unsetPaymentOption(PaymentOption $option, bool $refresh = true)
+ * @method ?PaymentOption getPaymentOption()
  */
 class Cart extends LunarCart
 {
     use HashesRouteKey;
+    use InteractsWithPaymentOptions;
 
     /**
      * Create a new instance of the Model.
@@ -38,53 +40,6 @@ class Cart extends LunarCart
             'paymentSubTotal',
             'paymentTotal',
         ]);
-    }
-
-    /**
-     * The applied payment option.
-     */
-    public ?PaymentOption $paymentOption = null;
-
-    /**
-     * The payment sub total for the cart.
-     */
-    public ?Price $paymentSubTotal = null;
-
-    /**
-     * The payment total for the cart.
-     */
-    public ?Price $paymentTotal = null;
-
-    /**
-     * Additional payment estimate meta data.
-     */
-    public array $paymentEstimateMeta = [];
-
-    /**
-     * All the payment breakdowns for the cart.
-     */
-    public ?PaymentBreakdown $paymentBreakdown = null;
-
-    /**
-     * Set the payment option to the shipping address.
-     */
-    public function setPaymentOption(PaymentOption $option, bool $refresh = true): Cart
-    {
-        foreach (Config::get('lunar.cart.validators.set_payment_option', []) as $action) {
-            App::make($action)->using(cart: $this, paymentOption: $option)->validate();
-        }
-
-        return App::make(Config::get('lunar.cart.actions.set_payment_option', SetPaymentOption::class))
-            ->execute($this, $option)
-            ->then(fn () => $refresh ? $this->refresh()->calculate() : $this);
-    }
-
-    /**
-     * Get the payment option for the cart
-     */
-    public function getPaymentOption(): ?PaymentOption
-    {
-        return PaymentManifest::getPaymentOption($this);
     }
 
     /**
