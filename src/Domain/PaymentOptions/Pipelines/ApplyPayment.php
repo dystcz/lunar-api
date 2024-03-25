48e5a5e333363c09e51ce80517d08db5d6c44309
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-03-25 20:32:25 +0100

    apply payment action wip

--- src/Domain/PaymentOptions/Pipelines/ApplyPayment.php
+++ src/Domain/PaymentOptions/Pipelines/ApplyPayment.php
@@ -3,6 +3,10 @@
 namespace Dystcz\LunarApi\Domain\PaymentOptions\Pipelines;
 
 use Closure;
+use Dystcz\LunarApi\Domain\Carts\ValueObjects\PaymentBreakdown;
+use Dystcz\LunarApi\Domain\Carts\ValueObjects\PaymentBreakdownItem;
+use Dystcz\LunarApi\Domain\PaymentOptions\Facades\PaymentManifest;
+use Lunar\DataTypes\Price;
 use Lunar\Models\Cart;
 
 class ApplyPayment
@@ -10,10 +14,37 @@ class ApplyPayment
     /**
      * Called just before cart totals are calculated.
      *
-     * @param  Closure(): void  $next
+     * @param  Closure(Cart): void  $next
      */
     public function handle(Cart $cart, Closure $next): void
     {
-        //
+        $paymentSubTotal = 0;
+        $paymentBreakdown = $cart->paymentBreakdown ?: new PaymentBreakdown;
+
+        $paymentOption = $cart->paymentOptionOverride ?: PaymentManifest::getPaymentOption($cart);
+
+        if ($paymentOption) {
+            $paymentBreakdown->items->put(
+                $paymentOption->getIdentifier(),
+                new PaymentBreakdownItem(
+                    name: $paymentOption->getName(),
+                    identifier: $paymentOption->getIdentifier(),
+                    price: $paymentOption->price,
+                )
+            );
+
+            $paymentSubTotal = $paymentOption->price->value;
+            $paymentTotal = $paymentSubTotal;
+        }
+
+        $cart->paymentBreakdown = $paymentBreakdown;
+
+        $cart->paymentSubTotal = new Price(
+            $paymentBreakdown->items->sum('price.value'),
+            $cart->currency,
+            1
+        );
+
+        $next($cart);
     }
 }
