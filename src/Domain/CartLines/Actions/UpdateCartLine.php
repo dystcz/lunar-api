e6935b080db9490b24e5ae1b88ab281735b353f2
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-03-26 03:05:53 +0100

    refactor action

--- src/Domain/CartLines/Actions/UpdateCartLine.php
+++ src/Domain/CartLines/Actions/UpdateCartLine.php
@@ -5,16 +5,25 @@ namespace Dystcz\LunarApi\Domain\CartLines\Actions;
 use Dystcz\LunarApi\Domain\CartLines\Data\CartLineData;
 use Dystcz\LunarApi\Domain\CartLines\Models\CartLine;
 use Dystcz\LunarApi\Domain\Carts\Models\Cart;
+use Illuminate\Support\Facades\App;
 use JetBrains\PhpStorm\ArrayShape;
-use Lunar\Facades\CartSession;
+use Lunar\Base\CartSessionInterface;
+use Lunar\Managers\CartSessionManager;
 
 class UpdateCartLine
 {
+    protected CartSessionManager $cartSession;
+
+    public function __construct(
+    ) {
+        $this->cartSession = App::make(CartSessionInterface::class);
+    }
+
     #[ArrayShape([Cart::class, CartLine::class])]
     public function __invoke(CartLineData $data, CartLine $cartLine): array
     {
         /** @var Cart $cart */
-        $cart = CartSession::current();
+        $cart = $this->cartSession->current();
 
         $cart = $cart->updateLine(
             cartLineId: $cartLine->id,
