69a41cb4175c428b4f08a761eac82802e90e0523
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-03-26 03:58:47 +0100

    add attach payment option test

---
+++ tests/Feature/Domain/Cart/JsonApi/V1/AttachPaymentOptionTest.php
@@ -0,0 +1,66 @@
+<?php
+
+use Dystcz\LunarApi\Domain\Carts\Models\Cart;
+use Dystcz\LunarApi\Domain\PaymentOptions\Facades\PaymentManifest;
+use Dystcz\LunarApi\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\App;
+use Lunar\Base\CartSessionInterface;
+
+uses(TestCase::class, RefreshDatabase::class);
+
+beforeEach(function () {
+    /** @var TestCase $this */
+    $this->cartSession = App::make(CartSessionInterface::class);
+
+    $this->cart = Cart::factory()->create();
+
+    $this->paymentOption = PaymentManifest::getOptions($this->cart)->first();
+});
+
+test('users can attach a payment option to cart address', function () {
+    /** @var TestCase $this */
+    $this->cartSession->use($this->cart);
+
+    $data = [
+        'type' => 'carts',
+        'attributes' => [
+            'payment_option' => $this->paymentOption->identifier,
+        ],
+    ];
+
+    $response = $this
+        ->jsonApi()
+        ->expects('carts')
+        ->withData($data)
+        ->post(serverUrl('/carts/-actions/attach-payment-option'));
+
+    $response
+        ->assertSuccessful()
+        ->assertFetchedOne($this->cart);
+
+    $this->assertDatabaseHas($this->cart->getTable(), [
+        'payment_option' => $this->paymentOption->identifier,
+    ]);
+
+    expect($this->cart->fresh()->payment_option)->toBe($data['attributes']['payment_option']);
+})->group('carts', 'payment-options');
+
+it('validates payment option attribute when attaching payment option to cart', function () {
+    /** @var TestCase $this */
+    $response = $this
+        ->jsonApi()
+        ->expects('carts')
+        ->withData([
+            'type' => 'carts',
+            'attributes' => [
+                'payment_option' => null,
+            ],
+        ])
+        ->post(serverUrl('/carts/-actions/attach-payment-option'));
+
+    $response->assertErrorStatus([
+        'detail' => __('lunar-api::validations.payments.attach_payment_option.payment_option.required'),
+        'status' => '422',
+    ]);
+})->group('carts', 'payment-options');
