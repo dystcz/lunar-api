8a715e93c805d10a036e476eac5da90c27b474a9
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-05-08 01:54:15 +0200

    add product variant relationships tests

---
+++ tests/Feature/Domain/ProductVariants/JsonApi/V1/ProductVariantOtherVariantsRelationshipTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Dystcz\LunarApi\Domain\Products\Models\Product;
+use Dystcz\LunarApi\Domain\ProductVariants\Factories\ProductVariantFactory;
+use Dystcz\LunarApi\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(TestCase::class, RefreshDatabase::class);
+
+it('can list other product variants through relationship', function () {
+    /** @var TestCase $this */
+    $variants = ProductVariantFactory::new()
+        ->for(Product::factory(), 'product')
+        ->count(3)
+        ->create();
+
+    $variant = $variants->first();
+    $otherVariants = $variants->where('id', '!=', $variant->id);
+
+    $response = $this
+        ->jsonApi()
+        ->expects('variants')
+        ->get(serverUrl("/variants/{$variant->getRouteKey()}/other_variants"));
+
+    $response
+        ->assertSuccessful()
+        ->assertFetchedMany($otherVariants)
+        ->assertDoesntHaveIncluded();
+})->group('variants');
