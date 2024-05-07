1a4c04bfb51b412c64ff6a7e0683d615f766c672
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-05-08 00:59:27 +0200

    add relationships to product variant route group

--- src/Domain/ProductVariants/Http/Routing/ProductVariantRouteGroup.php
+++ src/Domain/ProductVariants/Http/Routing/ProductVariantRouteGroup.php
@@ -6,6 +6,7 @@ use Dystcz\LunarApi\Domain\ProductVariants\Http\Controllers\ProductVariantsContr
 use Dystcz\LunarApi\Routing\Contracts\RouteGroup as RouteGroupContract;
 use Dystcz\LunarApi\Routing\RouteGroup;
 use LaravelJsonApi\Laravel\Facades\JsonApiRoute;
+use LaravelJsonApi\Laravel\Routing\Relationships;
 use LaravelJsonApi\Laravel\Routing\ResourceRegistrar;
 
 class ProductVariantRouteGroup extends RouteGroup implements RouteGroupContract
@@ -20,6 +21,16 @@ class ProductVariantRouteGroup extends RouteGroup implements RouteGroupContract
             ->resources(function (ResourceRegistrar $server) {
                 $server
                     ->resource($this->getPrefix(), ProductVariantsController::class)
+                    ->relationships(function (Relationships $relationships) {
+                        $relationships->hasOne('default_url')->readOnly();
+                        $relationships->hasMany('images')->readOnly();
+                        $relationships->hasMany('other_variants')->readOnly();
+                        $relationships->hasMany('prices')->readOnly();
+                        $relationships->hasMany('product')->readOnly();
+                        $relationships->hasOne('thumbnail')->readOnly();
+                        $relationships->hasMany('urls')->readOnly();
+                    })
+                    ->only('index', 'show')
                     ->readOnly();
             });
     }
