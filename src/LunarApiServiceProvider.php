993c447773cd3098c1f297b3629c376c10c13fe0
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-05-08 16:01:48 +0200

    add customers controller contract

--- src/LunarApiServiceProvider.php
+++ src/LunarApiServiceProvider.php
@@ -251,6 +251,7 @@ class LunarApiServiceProvider extends ServiceProvider
             \Dystcz\LunarApi\Domain\Collections\Contracts\CollectionsController::class => \Dystcz\LunarApi\Domain\Collections\Http\Controllers\CollectionsController::class,
             \Dystcz\LunarApi\Domain\Countries\Contracts\CountriesController::class => \Dystcz\LunarApi\Domain\Countries\Http\Controllers\CountriesController::class,
             \Dystcz\LunarApi\Domain\Currencies\Contracts\CurrenciesController::class => \Dystcz\LunarApi\Domain\Currencies\Http\Controllers\CurrenciesController::class,
+            \Dystcz\LunarApi\Domain\Customers\Contracts\CustomersController::class => \Dystcz\LunarApi\Domain\Customers\Http\Controllers\CustomersController::class,
         ];
 
         foreach ($controllers as $abstract => $concrete) {
