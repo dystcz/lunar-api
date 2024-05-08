aa492588b77792e54c22cd4186e80b15d3c8f399
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-05-09 00:19:10 +0200

    add lunar api config test

---
+++ tests/Unit/LunarApiConfigTest.php
@@ -0,0 +1,64 @@
+<?php
+
+use Dystcz\LunarApi\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\App;
+use Lunar\Facades\ModelManifest;
+
+uses(TestCase::class, RefreshDatabase::class);
+
+beforeEach(function () {
+    /** @var TestCase $this */
+});
+
+it('can list configured schemas', function () {
+    /** @var TestCase $this */
+
+    /** @var \Dystcz\LunarApi\LunarApiConfig $config */
+    $config = App::make('lunar-api-config');
+
+    $schemas = $config->getSchemas();
+
+    expect($schemas)->toBeInstanceOf(\Illuminate\Support\Collection::class);
+
+})->group('config');
+
+it('can list configured route groups', function () {
+    /** @var TestCase $this */
+
+    /** @var \Dystcz\LunarApi\LunarApiConfig $config */
+    $config = App::make('lunar-api-config');
+
+    $routes = $config->getRoutes();
+
+    expect($routes)->toBeInstanceOf(\Illuminate\Support\Collection::class);
+
+})->group('config');
+
+it('can list configured models for lunar model manifest', function () {
+    /** @var TestCase $this */
+
+    /** @var \Dystcz\LunarApi\LunarApiConfig $config */
+    $config = App::make('lunar-api-config');
+
+    $models = $config->getModels();
+
+    expect($models)->toBeInstanceOf(\Illuminate\Support\Collection::class);
+
+    $registeredModels = ModelManifest::getRegisteredModels();
+
+    $this->assertSame($models->toArray(), $registeredModels->toArray());
+
+})->group('config');
+
+it('can list configured policies', function () {
+    /** @var TestCase $this */
+
+    /** @var \Dystcz\LunarApi\LunarApiConfig $config */
+    $config = App::make('lunar-api-config');
+
+    $policies = $config->getPolicies();
+
+    expect($policies)->toBeInstanceOf(\Illuminate\Support\Collection::class);
+
+})->group('config');
