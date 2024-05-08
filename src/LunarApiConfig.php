99508f46925c1507b683c36a0bc2ecbedacb6cad
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-05-09 00:19:01 +0200

    add methods which get collections of configured classes from domain config

--- src/LunarApiConfig.php
+++ src/LunarApiConfig.php
@@ -3,6 +3,7 @@
 namespace Dystcz\LunarApi;
 
 use Dystcz\LunarApi\Support\Config\Collections\DomainConfigCollection;
+use Illuminate\Support\Collection;
 
 class LunarApiConfig
 {
@@ -11,5 +12,37 @@ class LunarApiConfig
     public function __construct()
     {
         $this->config = DomainConfigCollection::make();
+    }
+
+    /**
+     * Get schemas.
+     */
+    public function getSchemas(): Collection
+    {
+        return $this->config->getSchemas();
+    }
+
+    /**
+     * Get routes.
+     */
+    public function getRoutes(): Collection
+    {
+        return $this->config->getRoutes();
+    }
+
+    /**
+     * Get models for model manifest.
+     */
+    public function getModels(): Collection
+    {
+        return $this->config->getModels();
+    }
+
+    /**
+     * Get policies.
+     */
+    public function getPolicies(): Collection
+    {
+        return $this->config->getPolicies();
     }
 }
