d3a54e6857fe6714f5281fe5f5656e12ac675b42
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-05-09 00:18:01 +0200

    add docblocks

--- src/Facades/LunarApiConfig.php
+++ src/Facades/LunarApiConfig.php
@@ -4,14 +4,20 @@ namespace Dystcz\LunarApi\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Illuminate\Support\Collection getSchemas()
+ * @method static \Illuminate\Support\Collection getRoutes()
+ * @method static \Illuminate\Support\Collection getModels()
+ * @method static \Illuminate\Support\Collection getPolicies()
+ *
+ * @see \Dystcz\LunarApi\LunarApiConfig
+ */
 class LunarApiConfig extends Facade
 {
     /**
      * Get the registered name of the component.
-     *
-     * @return string
      */
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'lunar-api-config';
     }
