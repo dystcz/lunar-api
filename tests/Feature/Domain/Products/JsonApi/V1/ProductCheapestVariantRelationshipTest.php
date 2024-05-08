8973963df711f1e5bf34de27c819a66d2a299c62
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-05-08 02:35:03 +0200

    add product relationships tests

---
+++ tests/Feature/Domain/Products/JsonApi/V1/ProductCheapestVariantRelationshipTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Dystcz\LunarApi\Domain\Products\Models\Product;
+use Dystcz\LunarApi\Domain\ProductVariants\Factories\ProductVariantFactory;
+use Dystcz\LunarApi\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(TestCase::class, RefreshDatabase::class);
+
+it('can read cheapest variant through relationship', function () {
+    /** @var TestCase $this */
+    $product = Product::factory()
+        ->has(ProductVariantFactory::new()->withPrice()->count(4), 'variants')
+        ->create();
+
+    $response = $this
+        ->jsonApi()
+        ->expects('variants')
+        ->get(serverUrl("/products/{$product->getRouteKey()}/cheapest_variant"));
+
+    $response
+        ->assertSuccessful()
+        ->assertFetchedOne(
+            $product->variants->sortBy(
+                fn ($variant) => $variant->prices->sortBy('price')->first()->price,
+            )->first(),
+        )
+        ->assertDoesntHaveIncluded();
+})->group('products');
---
+++ tests/Feature/Domain/Products/JsonApi/V1/ProductLowestPriceRelationshipTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Dystcz\LunarApi\Domain\Products\Factories\ProductFactory;
+use Dystcz\LunarApi\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(TestCase::class, RefreshDatabase::class);
+
+it('can read lowest price through relationship', function () {
+    /** @var TestCase $this */
+    $product = ProductFactory::new()
+        ->withPrices(3)
+        ->create();
+
+    $response = $this
+        ->jsonApi()
+        ->expects('prices')
+        ->get(serverUrl("/products/{$product->getRouteKey()}/lowest_price"));
+
+    $response
+        ->assertSuccessful()
+        ->assertFetchedOne($product->lowestPrice)
+        ->assertDoesntHaveIncluded();
+})->group('products');
