8c18fb48968bf7f958ce698a19d0186ee826044c
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-05-15 21:02:40 +0200

    add product specific availability trait

---
+++ src/Domain/Products/Traits/InteractsWithAvailability.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Dystcz\LunarApi\Domain\Products\Traits;
+
+use Dystcz\LunarApi\Base\Traits\InteractsWithAvailability as BaseInteractsWithAvailability;
+use Dystcz\LunarApi\Domain\Products\Enums\Availability;
+use Dystcz\LunarApi\Domain\Products\Models\Product;
+use Dystcz\LunarApi\Domain\ProductVariants\Models\ProductVariant;
+
+trait InteractsWithAvailability
+{
+    use BaseInteractsWithAvailability;
+
+    /**
+     * Determine when model is considered to be always purchasable.
+     */
+    public function isAlwaysPurchasable(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine when model is considered to be backorderable.
+     */
+    public function isBackorderable(): bool
+    {
+        /** @var Product $this */
+        $availability = $this->getAttribute('variants_availability');
+
+        if ($availability->contains(Availability::BACKORDER)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine when model is considered to be in stock.
+     */
+    public function isInStock(): bool
+    {
+        /** @var Product $this */
+        $availability = $this->getAttribute('variants_availability');
+
+        if ($availability->contains(Availability::IN_STOCK) || $availability->contains(Availability::ALWAYS)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Prepare model for availability evaluation.
+     */
+    public function prepareModelForAvailabilityEvaluation(): void
+    {
+        /** @var Product $this */
+        $this->setAttribute(
+            'variants_availability',
+            $this->variants->map(fn (ProductVariant $variant) => $variant->getAvailability()),
+        );
+    }
+}
