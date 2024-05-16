e7726201f3d14c46f672cc56e013a710df45c274
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-05-16 19:38:21 +0200

    implement translatable contract to models with translatable attributes
    implement contract

--- src/Domain/AttributeGroups/Models/AttributeGroup.php
+++ src/Domain/AttributeGroups/Models/AttributeGroup.php
@@ -2,10 +2,11 @@
 
 namespace Dystcz\LunarApi\Domain\AttributeGroups\Models;
 
+use Dystcz\LunarApi\Base\Contracts\Translatable;
 use Dystcz\LunarApi\Hashids\Traits\HashesRouteKey;
 use Lunar\Models\AttributeGroup as LunarAttributeGroup;
 
-class AttributeGroup extends LunarAttributeGroup
+class AttributeGroup extends LunarAttributeGroup implements Translatable
 {
     use HashesRouteKey;
 }
--- src/Domain/ProductOptionValues/Models/ProductOptionValue.php
+++ src/Domain/ProductOptionValues/Models/ProductOptionValue.php
@@ -2,11 +2,12 @@
 
 namespace Dystcz\LunarApi\Domain\ProductOptionValues\Models;
 
+use Dystcz\LunarApi\Base\Contracts\Translatable;
 use Dystcz\LunarApi\Domain\ProductOptionValues\Factories\ProductOptionValueFactory;
 use Dystcz\LunarApi\Hashids\Traits\HashesRouteKey;
 use Lunar\Models\ProductOptionValue as LunarProductOptionValue;
 
-class ProductOptionValue extends LunarProductOptionValue
+class ProductOptionValue extends LunarProductOptionValue implements Translatable
 {
     use HashesRouteKey;
 
