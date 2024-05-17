2aa55fe2c72efd172b4e34f15b268955ca213e29
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-05-17 19:53:29 +0200

    fix label

--- src/Domain/ProductOptions/Models/ProductOption.php
+++ src/Domain/ProductOptions/Models/ProductOption.php
@@ -5,6 +5,7 @@ namespace Dystcz\LunarApi\Domain\ProductOptions\Models;
 use Dystcz\LunarApi\Base\Contracts\Translatable;
 use Dystcz\LunarApi\Domain\ProductOptions\Factories\ProductOptionFactory;
 use Dystcz\LunarApi\Hashids\Traits\HashesRouteKey;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Lunar\Models\ProductOption as LunarProductOption;
 
 class ProductOption extends LunarProductOption implements Translatable
@@ -17,5 +18,16 @@ class ProductOption extends LunarProductOption implements Translatable
     protected static function newFactory(): ProductOptionFactory
     {
         return ProductOptionFactory::new();
+    }
+
+    /**
+     * Get label attribute.
+     */
+    protected function label(): Attribute
+    {
+        return Attribute::make(
+            get: fn (?string $value = null) => $value ? json_decode($value) : [],
+            set: fn ($value) => json_encode($value),
+        );
     }
 }
