a59b68a329bb5da07ee1404bd44bc5a6ca257cb1
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-05-20 18:34:07 +0200

    finish cart contract tests

--- tests/Unit/Domain/Carts/Contracts/CartTest.php
+++ tests/Unit/Domain/Carts/Contracts/CartTest.php
@@ -1,10 +1,17 @@
 <?php
 
+use Dystcz\LunarApi\Domain\Carts\Contracts\CurrentSessionCart;
+use Dystcz\LunarApi\Domain\Carts\Models\Cart;
 use Dystcz\LunarApi\Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\App;
 
 uses(TestCase::class, RefreshDatabase::class);
 
 it('can bind cart contract to model implementation', function () {
+    /** @var TestCase $this */
+    $cart = App::make(CurrentSessionCart::class);
 
-})->group('carts', 'carts.model')->todo();
+    $this->assertInstanceOf(Cart::class, $cart);
+
+})->group('carts', 'carts.model');
--- tests/Unit/Domain/Carts/Contracts/CurrentSessionCart.php
+++ tests/Unit/Domain/Carts/Contracts/CurrentSessionCart.php
@@ -1,10 +1,32 @@
 <?php
 
+use Dystcz\LunarApi\Domain\Carts\Contracts\CurrentSessionCart;
+use Dystcz\LunarApi\Domain\Carts\Factories\CartFactory;
+use Dystcz\LunarApi\Domain\Carts\Models\Cart;
 use Dystcz\LunarApi\Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Lunar\Base\CartSessionInterface;
 
 uses(TestCase::class, RefreshDatabase::class);
 
 it('can get current cart from session when resolving contract', function () {
+    /** @var Cart $cart */
+    $carts = CartFactory::new()
+        ->withAddresses()
+        ->withLines()
+        ->count(3)
+        ->create();
 
-})->group('carts', 'carts.model')->todo();
+    $sessionCart = array_rand($carts);
+
+    /** @var \Lunar\Managers\CartSessionManager $cartSession */
+    $cartSession = App::make(CartSessionInterface::class);
+
+    $cartSession->use($sessionCart);
+
+    /** @var TestCase $this */
+    $cart = App::make(CurrentSessionCart::class);
+
+    $this->assertInstanceOf(Cart::class, $sessionCart);
+
+})->group('carts', 'carts.model');
