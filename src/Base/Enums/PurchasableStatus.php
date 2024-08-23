59e6d49a345cb646d02fa98917a7b49d22ddeb4e
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-08-23 14:08:50 +0200

    change backorder enum value

--- src/Base/Enums/PurchasableStatus.php
+++ src/Base/Enums/PurchasableStatus.php
@@ -6,5 +6,5 @@ enum PurchasableStatus: string
 {
     case ALWAYS = 'always';
     case IN_STOCK = 'in_stock';
-    case BACKORDER = 'backorder';
+    case BACKORDER = 'in_stock_or_on_backorder';
 }
