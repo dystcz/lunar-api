a0924bb242777f81d652cdf07fd8d6728a1985aa
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-08-30 23:01:41 +0200

    add create order lines pipeline (wip)

---
+++ src/Domain/Orders/Pipelines/CreateOrderLines.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Dystcz\LunarApi\Domain\Orders\Pipelines;
+
+use Closure;
+use Dystcz\LunarApi\Domain\Orders\Models\Order;
+use Lunar\Facades\ModelManifest;
+use Lunar\Models\Contracts\Order as OrderContract;
+use Lunar\Models\Contracts\OrderLine as OrderLineContract;
+use Lunar\Pipelines\Order\Creation\CreateOrderLines as LunarCreateOrderLines;
+
+class CreateOrderLines extends LunarCreateOrderLines
+{
+    /**
+     * @param  Closure(OrderContract): mixed  $next
+     * @return mixed
+     */
+    public function handle(OrderContract $order, Closure $next)
+    {
+        /** @var Order $order */
+        if (! $order->id) {
+            $order->save();
+        }
+
+        $cart = $order->cart;
+
+        $cart->recalculate();
+
+        $orderLineClass = ModelManifest::get(OrderLineContract::class);
+
+        foreach ($cart->lines as $cartLine) {
+            $orderLine = $order->lines->first(function ($line) use ($cartLine) {
+                return $line->purchasable_id == $cartLine->purchasable_id &&
+                    $line->purchasable_type == $cartLine->purchasable_type;
+            }) ?: new $orderLineClass;
+
+            $orderLine->fill([
+                'order_id' => $order->id,
+                'purchasable_type' => $cartLine->purchasable_type,
+                'purchasable_id' => $cartLine->purchasable_id,
+                'type' => $cartLine->purchasable->getType(),
+                'description' => $cartLine->purchasable->getDescription(),
+                'option' => $cartLine->purchasable->getOption(),
+                'identifier' => $cartLine->purchasable->getIdentifier(),
+                'unit_price' => $cartLine->unitPrice->value,
+                'unit_quantity' => $cartLine->purchasable->getUnitQuantity(),
+                'quantity' => $cartLine->quantity,
+                'sub_total' => $cartLine->subTotal->value,
+                'discount_total' => $cartLine->discountTotal?->value,
+                'tax_breakdown' => $cartLine->taxBreakdown,
+                'tax_total' => $cartLine->taxAmount->value,
+                'total' => $cartLine->total->value,
+                'notes' => null,
+                'meta' => $cartLine->meta,
+            ])->save();
+        }
+
+        return $next($order->refresh());
+    }
+}
