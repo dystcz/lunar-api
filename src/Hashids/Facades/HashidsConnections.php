6f59a61e9b1f06ac16f33435d1e92759e9f9537b
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-08-30 18:07:00 +0200

    remove nonsense docblock

--- src/Hashids/Facades/HashidsConnections.php
+++ src/Hashids/Facades/HashidsConnections.php
@@ -3,18 +3,8 @@
 namespace Dystcz\LunarApi\Hashids\Facades;
 
 use Dystcz\LunarApi\Hashids\Contracts\HashidsConnectionsManager;
-use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
 
-/**
- * Class ModelManifest.
- *
- * @method static void registerConnections(Collection $models)
- * @method static \Illuminate\Support\Collection getConnections()
- * @method static ?string getModelConnection(string $model)
- *
- * @see \Lunar\Base\ModelManifest
- */
 class HashidsConnections extends Facade
 {
     /**
