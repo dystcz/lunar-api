d949b01327060a32768bf6b5c1b134dbb7ec5979
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-09-02 17:42:01 +0200

    rename contract

--- src/Domain/Media/Contracts/MediaConversions.php
+++ src/Domain/Media/Contracts/MediaConversions.php
@@ -5,12 +5,12 @@ namespace Dystcz\LunarApi\Domain\Media\Contracts;
 use Dystcz\LunarApi\Domain\Media\Data\ConversionOptions;
 use Spatie\MediaLibrary\HasMedia;
 
-interface MediaConversion
+interface MediaConversions
 {
     /**
      * Apply conversions to a model.
      */
-    public function apply(HasMedia $model): void;
+    public static function apply(HasMedia $model): void;
 
     /**
      * Get conversions.
