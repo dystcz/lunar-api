f11ee832fdd2e6ad34792e6f6ec4618291494bb4
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-09-14 03:37:58 +0200

    update attributes

--- src/Domain/Attributes/Traits/InteractsWithAttributes.php
+++ src/Domain/Attributes/Traits/InteractsWithAttributes.php
@@ -2,35 +2,23 @@
 
 namespace Dystcz\LunarApi\Domain\Attributes\Traits;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Lunar\Models\Attribute as LunarAttribute;
 
 trait InteractsWithAttributes
 {
     /**
-     * Getter to return the class name used with attribute relationships.
-     *
-     * @return string
-     */
-    public function attributeClassname(): Attribute
-    {
-        /** @var \Illuminate\Database\Eloquent\Model $this */
-
-        return Attribute::make(
-            get: fn () => $this->getMorphClass(),
-        );
-    }
-
-    /**
      * Get the mapped attributes relation.
+     *
+     * @return HasMany<LunarAttribute,Model>
      */
     public function attributes(): HasMany
     {
         /** @var \Illuminate\Database\Eloquent\Model $this */
 
         return $this->hasMany(
-            LunarAttribute::class,
+            LunarAttribute::modelClass(),
             'attribute_type',
             'attribute_classname',
         );
