dd8aefc9f15cc1f87bd498e920602e6b8f5d48c1
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-09-14 22:58:29 +0200

    extract tax zone logic to a trait

--- src/Domain/TaxZones/Models/TaxZone.php
+++ src/Domain/TaxZones/Models/TaxZone.php
@@ -2,20 +2,10 @@
 
 namespace Dystcz\LunarApi\Domain\TaxZones\Models;
 
+use Dystcz\LunarApi\Domain\TaxZones\Concerns\InteractsWithLunarApi;
 use Lunar\Models\TaxZone as LunarTaxZone;
-use Spatie\LaravelBlink\BlinkFacade;
 
 class TaxZone extends LunarTaxZone
 {
-    /**
-     * Get the default tax percentage.
-     */
-    public static function getDefaultPercentage(): float
-    {
-        $key = 'lunar_default_tax_zone_percentage';
-
-        return BlinkFacade::once($key, function () {
-            return floatval(static::getDefault()->taxAmounts->first()?->percentage);
-        });
-    }
+    use InteractsWithLunarApi;
 }
