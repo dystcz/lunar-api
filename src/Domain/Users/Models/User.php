9a05d66abbdaa9e6ce01c31f17773c1afc542a41
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-09-14 03:39:42 +0200

    update user model

--- src/Domain/Users/Models/User.php
+++ src/Domain/Users/Models/User.php
@@ -3,9 +3,10 @@
 namespace Dystcz\LunarApi\Domain\Users\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Lunar\Base\LunarUser as LunarUserContract;
 use Lunar\Base\Traits\LunarUser;
 
-class User extends Authenticatable
+class User extends Authenticatable implements LunarUserContract
 {
     use LunarUser;
 
