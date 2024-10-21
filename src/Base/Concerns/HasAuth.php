edf0462f3d2bc81d0cda1507db8edfac16cb2fcc
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-10-21 19:01:41 +0200

    move logic to concerns

--- src/Base/Concerns/HasAuth.php
+++ src/Base/Concerns/HasAuth.php
@@ -2,7 +2,12 @@
 
 namespace Dystcz\LunarApi\Base\Concerns;
 
+use Dystcz\LunarApi\Domain\Carts\Contracts\CheckoutCart;
+use Dystcz\LunarApi\Domain\Users\Contracts\CreatesNewUsers;
+use Dystcz\LunarApi\Domain\Users\Contracts\CreatesUserFromCart;
+use Dystcz\LunarApi\Domain\Users\Contracts\RegistersUser;
 use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
 
 trait HasAuth
@@ -24,5 +29,45 @@ trait HasAuth
     public function getAuthGuard(): string
     {
         return $this->authGuard;
+    }
+
+    /**
+     * Create new user using class.
+     */
+    public function createUserUsing(string $class): static
+    {
+        App::singleton(CreatesNewUsers::class, $class);
+
+        return $this;
+    }
+
+    /**
+     * Create user from cart using class.
+     */
+    public function createUserFromCartUsing(string $class): static
+    {
+        App::singleton(CreatesUserFromCart::class, $class);
+
+        return $this;
+    }
+
+    /**
+     * Register user using class.
+     */
+    public function registerUserUsing(string $class): static
+    {
+        App::singleton(RegistersUser::class, $class);
+
+        return $this;
+    }
+
+    /**
+     * Checkout cart using class.
+     */
+    public function checkoutCartUsing(string $class): static
+    {
+        App::singleton(CheckoutCart::class, $class);
+
+        return $this;
     }
 }
--- src/LunarApi.php
+++ src/LunarApi.php
@@ -3,62 +3,9 @@
 namespace Dystcz\LunarApi;
 
 use Dystcz\LunarApi\Base\Concerns;
-use Dystcz\LunarApi\Domain\Carts\Contracts\CheckoutCart;
-use Dystcz\LunarApi\Domain\Users\Contracts\CreatesNewUsers;
-use Dystcz\LunarApi\Domain\Users\Contracts\CreatesUserFromCart;
-use Dystcz\LunarApi\Domain\Users\Contracts\RegistersUser;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Config;
 
 class LunarApi
 {
     use Concerns\HasAuth;
-
-    /**
-     * Create new user using class.
-     */
-    public function createUserUsing(string $class): static
-    {
-        App::singleton(CreatesNewUsers::class, $class);
-
-        return $this;
-    }
-
-    /**
-     * Create user from cart using class.
-     */
-    public function createUserFromCartUsing(string $class): static
-    {
-        App::singleton(CreatesUserFromCart::class, $class);
-
-        return $this;
-    }
-
-    /**
-     * Register user using class.
-     */
-    public function registerUserUsing(string $class): static
-    {
-        App::singleton(RegistersUser::class, $class);
-
-        return $this;
-    }
-
-    /**
-     * Checkout cart using class.
-     */
-    public function checkoutCartUsing(string $class): static
-    {
-        App::singleton(CheckoutCart::class, $class);
-
-        return $this;
-    }
-
-    /**
-     * Check if hashids are used.
-     */
-    public function usesHashids(): bool
-    {
-        return Config::get('lunar-api.general.use_hashids', false);
-    }
+    use Concerns\HashesIds;
 }
