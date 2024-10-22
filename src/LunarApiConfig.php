d3af869afd72c9ea8c4a86226eb832e4ebc3ba58
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-10-22 04:52:08 +0200

    add domain methods

--- src/LunarApiConfig.php
+++ src/LunarApiConfig.php
@@ -3,44 +3,49 @@
 namespace Dystcz\LunarApi;
 
 use Dystcz\LunarApi\Support\Config\Collections\DomainConfigCollection;
+use Dystcz\LunarApi\Support\Config\Data\DomainConfig;
 use Illuminate\Support\Collection;
 
 class LunarApiConfig
 {
     public DomainConfigCollection $config;
 
+    /** @param array<string,DomainConfig> $domains */
+    public array $domains = [];
+
     public function __construct()
     {
         $this->config = DomainConfigCollection::make();
     }
 
+    public function domain(string $domain): DomainConfig
+    {
+        return $this->config->get($domain);
+    }
+
     /**
-     * Get schemas.
+     * @param  string[]  $domains
      */
+    public function domains(...$domains): DomainConfig
+    {
+        return $this->config->only($domains);
+    }
+
     public function getSchemas(): Collection
     {
         return $this->config->getSchemas();
     }
 
-    /**
-     * Get routes.
-     */
     public function getRoutes(): Collection
     {
         return $this->config->getRoutes();
     }
 
-    /**
-     * Get models for model manifest.
-     */
     public function getModels(): Collection
     {
         return $this->config->getModelsForModelManifest();
     }
 
-    /**
-     * Get policies.
-     */
     public function getPolicies(): Collection
     {
         return $this->config->getPolicies();
